ad587c9db49d6fc1fbe56b571bd03b5203fddec0
Atam Jacob <jacob.atam@gmail.com>
2022-06-08 09:51:40 -0700

    Updated Ui

--- app/Models/activity.php
+++ app/Models/activity.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class activity extends Model
+class Activity extends Model
 {
     use HasFactory;
 }
