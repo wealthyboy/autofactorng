7ceff5f67a9b0e8093468132b3f9c2911953f636
Seth Atam <seth.atam@gmail.com>
2022-07-20 17:56:00 +0100

    Updated

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -306,7 +306,7 @@
                                 <i class="fa fa-pencil"></i> Edit</a>
                           </label>
                       </div> 
-                      @include('includes.children',['obj'=>$attribute,'space'=>'&nbsp;&nbsp;','model' => 'attributes','url' => 'attribute','year' => true, 'name' => 'attribute_id','engine' => true, 'route' => 'attributes']])
+                      @include('includes.children',['obj'=>$attribute,'space'=>'&nbsp;&nbsp;','model' => 'attributes','url' => 'attribute','year' => true, 'name' => 'attribute_id','engine' => true, 'route' => 'attributes'])
                   </div>
                   @endforeach  
                </div>
