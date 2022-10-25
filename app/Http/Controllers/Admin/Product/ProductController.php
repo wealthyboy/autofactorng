eb93fa4b89f9920de2a09fa76a3a531857daf74e
Seth Atam <seth.atam@gmail.com>
2022-10-25 15:44:32 +0100

    Debug

--- app/Http/Controllers/Admin/Product/ProductController.php
+++ app/Http/Controllers/Admin/Product/ProductController.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 
 namespace App\Http\Controllers\Admin\Product;
@@ -25,12 +25,12 @@ use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
-    
+
     protected $settings;
 
     public function __construct()
-    {	  
-	  $this->settings =  Setting::first();
+    {
+        $this->settings =  Setting::first();
     }
 
     /**
@@ -39,41 +39,41 @@ class ProductController extends Controller
      * return \Illuminate\Http\Response
      */
     public function index()
-    {    
-       
+    {
+
         $brands     = Brand::all();
         $categories = Category::parents()->get();
-        $attributes = Attribute::parents()->orderBy('sort_order','asc')->get();
+        $attributes = Attribute::parents()->orderBy('sort_order', 'asc')->get();
         $years      = Helper::years();
         $products   = Product::with('categories')
-                           ->orderBy('created_at','desc')->paginate(100);
+            ->orderBy('created_at', 'desc')->paginate(100);
         if (request()->filled('q')) {
             $value = request()->q;
-            $products = Product::where('name', 'like', '%' .$value . '%')
-                                ->latest()->paginate(100);
+            $products = Product::where('name', 'like', '%' . $value . '%')
+                ->latest()->paginate(100);
             $products->appends(request()->query());
         }
-        
-        return view('admin.products.index',compact('products','brands','categories','attributes','years'));
+
+        return view('admin.products.index', compact('products', 'brands', 'categories', 'attributes', 'years'));
     }
 
 
-   
-    
+
+
 
     public function loadAttr(Request $request)
     {
         $attribute_id = array_filter($request->attribute_ids);
-        if (empty($attribute_id)){
+        if (empty($attribute_id)) {
             return response()->json([
                 'error' => 'Please select at least 1 attribute'
-            ],422);
+            ], 422);
         }
         $product_attributes = Attribute::find($attribute_id);
-        $counter = rand(1,500);
-        return view('admin.products.variation',compact('counter','product_attributes'));
+        $counter = rand(1, 500);
+        return view('admin.products.variation', compact('counter', 'product_attributes'));
     }
-    
+
 
     /**
      * Show the form for creating a new resource.
@@ -85,11 +85,11 @@ class ProductController extends Controller
         //User::canTakeAction(2);
         $brands = Brand::all();
         $categories = Category::parents()->get();
-        $attributes = Attribute::parents()->orderBy('sort_order','asc')->get();
+        $attributes = Attribute::parents()->orderBy('sort_order', 'asc')->get();
         $years = Helper::years();
         $helper = new Helper;
         $amps = Product::AMPHERES;
-        return view('admin.products.create',compact('amps','brands','categories','attributes','years','helper'));
+        return view('admin.products.create', compact('amps', 'brands', 'categories', 'attributes', 'years', 'helper'));
     }
 
     /**
@@ -98,8 +98,8 @@ class ProductController extends Controller
      * param  \Illuminate\Http\Request  $request
      * return \Illuminate\Http\Response
      */
-    public function store(Request $request,Product $product)
-    {   
+    public function store(Request $request, Product $product)
+    {
 
         $validator = Validator::make($request->all(), [
             'category_id' => 'required',
@@ -110,19 +110,19 @@ class ProductController extends Controller
         $data = [];
         $attr = [];
         if (!empty($request->attribute_id)) {
-            $attributes = Attribute::whereIn('id',$request->attribute_id)->where('parent_id', null)->get();
+            $attributes = Attribute::whereIn('id', $request->attribute_id)->where('parent_id', null)->get();
             if (null == $attributes) {
                 $data[] = 'You need to add at least 1 make for your upload';
             } else {
                 foreach ($attributes as $key => $attribute) {
                     if ($attribute->children->containsOneItem($request->attribute_id)) {
-                        $data[] = 'No model for '.$attribute->name;
+                        $data[] = 'No model for ' . $attribute->name;
                     }
                 }
             }
 
-            if ( !empty( $request->year_from ) ) {
-                foreach( $request->year_from as $attribute_id => $year) {
+            if (!empty($request->year_from)) {
+                foreach ($request->year_from as $attribute_id => $year) {
                     $attr[] = $attribute_id;
                 }
             }
@@ -130,7 +130,8 @@ class ProductController extends Controller
 
         if ($validator->fails()) {
             $validator->errors()->add(
-                'extras', $data
+                'extras',
+                $data
             );
             return response()->json(array(
                 'success' => false,
@@ -138,42 +139,43 @@ class ProductController extends Controller
             ), 400);
         }
 
-       //dd($attributes);
+        //dd($attributes);
 
 
         $data = $request->except('_token');
         $brand = Brand::find($request->brand_id);
         $data['quantity'] = 1;
         $category = Category::find($request->category_id);
-        $name = $request->filled('brand_id') ? $brand->name .' '.$request->product_name  : $request->product_name;
+        $name = $request->filled('brand_id') ? $brand->name . ' ' . $request->product_name  : $request->product_name;
         $data['name'] = $name;
         $data['product_name'] = $request->product_name;
         $data['slug'] = str_slug($name);
+        dd($data);
         $product = Product::create($data);
-        
+
         if (!empty($request->category_id)) {
             $product->categories()->sync($request->category_id);
         }
 
-        if ( !empty($request->engine_id) ) {
+        if (!empty($request->engine_id)) {
             $data = [];
             foreach ($request->engine_id as $key => $engines) {
                 foreach ($engines as $engine) {
-                  $e =  new EngineProduct;
-                  $e->product_id =$product->id ;
-                  $e->engine_id = $engine;
-                  $e->attribute_id = $key ;
-                  $e->save();
+                    $e =  new EngineProduct;
+                    $e->product_id = $product->id;
+                    $e->engine_id = $engine;
+                    $e->attribute_id = $key;
+                    $e->save();
                 }
             }
         }
 
         $product->attributes()->sync($request->attribute_id);
-        
+
 
         if ($request->condition_is_present) {
 
-            foreach( $request->condition['lagos']['tag'] as $key => $value) {
+            foreach ($request->condition['lagos']['tag'] as $key => $value) {
                 $shipping_rate = new ShippingRate;
                 $shipping_rate->tag = $request->condition['lagos']['tag'][$key];
                 $shipping_rate->condition = $request->condition['lagos']['condition'][$key];
@@ -184,7 +186,7 @@ class ProductController extends Controller
                 $shipping_rate->save();
             }
 
-            foreach( $request->condition['out_side_lagos']['tag'] as $key => $value) {
+            foreach ($request->condition['out_side_lagos']['tag'] as $key => $value) {
                 $shipping_rate = new ShippingRate;
                 $shipping_rate->tag = $request->condition['out_side_lagos']['tag'][$key];
                 $shipping_rate->condition = $request->condition['out_side_lagos']['condition'][$key];
@@ -196,7 +198,7 @@ class ProductController extends Controller
             }
         }
 
-        foreach( $request->year_from as $attribute_id => $year) {
+        foreach ($request->year_from as $attribute_id => $year) {
             $product_year = new ProductYear;
             $product_year->attribute_id = $attribute_id;
             $product_year->product_id = $product->id;
@@ -204,29 +206,29 @@ class ProductController extends Controller
             $product_year->save();
         }
 
-        foreach( $request->year_to as $attribute_id => $year) {
+        foreach ($request->year_to as $attribute_id => $year) {
             $product_year = ProductYear::where(['attribute_id' => $attribute_id, 'product_id' => $product->id])->first();
             $product_year->year_to = $year;
             $product_year->save();
         }
 
-    
-       // $data = json_encode($product->toArray());
 
-        if ( !empty($request->images) ) {
+        // $data = json_encode($product->toArray());
+
+        if (!empty($request->images)) {
             $images =  $request->images;
-            foreach ( $images as $image) {
+            foreach ($images as $image) {
                 $images = new Image(['image' => $image]);
                 $product->images()->save($images);
             }
-        } 
+        }
 
         //(new Activity)->Log("Added a product ", "{$data}");
         return response()->json($product);
     }
 
 
-     /**
+    /**
      * Show the form for creating a new resource.
      *
      * return \Illuminate\Http\Response
@@ -237,52 +239,51 @@ class ProductController extends Controller
         $brands = Brand::all();
         $product = Product::find($id);
         $categories = Category::parents()->get();
-        $attributes = Attribute::parents()->orderBy('sort_order','asc')->get();
+        $attributes = Attribute::parents()->orderBy('sort_order', 'asc')->get();
         $years = Helper::years();
         $helper = new Helper;
         $year_from = $product->product_years->pluck('year_from')->toArray();
         $year_to = $product->product_years->pluck('year_to')->toArray();
         $amps = Product::AMPHERES;
-        return view('admin.products.edit',compact('amps','product','brands','categories','year_from','year_to','attributes','years','helper'));
+        return view('admin.products.edit', compact('amps', 'product', 'brands', 'categories', 'year_from', 'year_to', 'attributes', 'years', 'helper'));
     }
 
 
-    public function search(Request $request) 
+    public function search(Request $request)
     {
 
         $filtered_array = $request->only(['q', 'field']);
-		if (empty( $filtered_array['q'] ) )  { 
-			return redirect('/errors');
-		}
-		if($request->has('q')){
-			$filtered_array = array_filter($filtered_array);
-            $query = Product::
-                whereHas('categories', function( $query ) use ( $filtered_array ){
-                $query->where('categories.name','like','%' .$filtered_array['q'] . '%')
-                    ->orWhere('products.product_name', 'like', '%' .$filtered_array['q'] . '%')
-                    ->orWhere('products.sku', 'like', '%' .$filtered_array['q'] . '%');
-            })->orWhereHas('variants', function( $query ) use ( $filtered_array ){
-                $query->where('product_variations.name', 'like', '%' .$filtered_array['q'] . '%')
-                ->orWhere('product_variations.sku', 'like', '%' .$filtered_array['q'] . '%');
-            });
+        if (empty($filtered_array['q'])) {
+            return redirect('/errors');
         }
-			
+        if ($request->has('q')) {
+            $filtered_array = array_filter($filtered_array);
+            $query = Product::whereHas('categories', function ($query) use ($filtered_array) {
+                    $query->where('categories.name', 'like', '%' . $filtered_array['q'] . '%')
+                        ->orWhere('products.product_name', 'like', '%' . $filtered_array['q'] . '%')
+                        ->orWhere('products.sku', 'like', '%' . $filtered_array['q'] . '%');
+                })->orWhereHas('variants', function ($query) use ($filtered_array) {
+                    $query->where('product_variations.name', 'like', '%' . $filtered_array['q'] . '%')
+                        ->orWhere('product_variations.sku', 'like', '%' . $filtered_array['q'] . '%');
+                });
+        }
+
         $products = $query->groupBy('products.id')->paginate(10);
         $products->appends(request()->all());
-        return view('admin.products.index',compact('products'));  
+        return view('admin.products.index', compact('products'));
     }
 
 
     public function acMessage($product)
     {
 
-        $data =null;
-        foreach( $product->product_variations as $product_variation ) {
+        $data = null;
+        foreach ($product->product_variations as $product_variation) {
             $data  = '<p>';
-            $data .= 'Name: '. $product_variation->name  .' <br/>';
-            $data .= 'Qty: '. $product_variation->quantity . '<br/>';
-            $data .= 'Price: '. $product_variation->price . '<br/>';
-            $data .= 'Sale Price: '. $product_variation->sale_price . '<br/>';
+            $data .= 'Name: ' . $product_variation->name  . ' <br/>';
+            $data .= 'Qty: ' . $product_variation->quantity . '<br/>';
+            $data .= 'Price: ' . $product_variation->price . '<br/>';
+            $data .= 'Sale Price: ' . $product_variation->sale_price . '<br/>';
             $data .= '</p>';
         }
         return $data;
@@ -290,9 +291,10 @@ class ProductController extends Controller
 
 
 
-    public function update(Request $request,$id){
-        
-        $this->validate($request,[
+    public function update(Request $request, $id)
+    {
+
+        $this->validate($request, [
             'category_id' => 'required',
             'product_name' => 'required',
         ]);
@@ -301,12 +303,12 @@ class ProductController extends Controller
         $brand = Brand::find($request->brand_id);
         $data['quantity'] = 1;
         $category = Category::find($request->category_id);
-        $name = $request->filled('brand_id')  ? $brand->name .' '.$request->product_name  : $request->product_name;
+        $name = $request->filled('brand_id')  ? $brand->name . ' ' . $request->product_name  : $request->product_name;
         $data['name'] = $name;
         $data['slug'] = str_slug($name);
         $product = Product::find($id);
         $product->update($data);
-        
+
         if (!empty($request->category_id)) {
             $product->categories()->sync($request->category_id);
         }
@@ -316,21 +318,21 @@ class ProductController extends Controller
             $product->product_engines()->delete();
         }
 
-        if ( !empty($request->engine_id) ) {
+        if (!empty($request->engine_id)) {
             $data = [];
             foreach ($request->engine_id as $key => $engines) {
                 foreach ($engines as $engine) {
-                  $e =  new EngineProduct;
-                  $e->product_id =$product->id ;
-                  $e->engine_id = $engine;
-                  $e->attribute_id = $key ;
-                  $e->save();
+                    $e =  new EngineProduct;
+                    $e->product_id = $product->id;
+                    $e->engine_id = $engine;
+                    $e->attribute_id = $key;
+                    $e->save();
                 }
             }
         }
 
         $product->attributes()->sync($request->attribute_id);
-        
+
 
         //Delete prwvious record
         if (null !== $product->product_rates) {
@@ -339,7 +341,7 @@ class ProductController extends Controller
 
         if ($request->condition_is_present) {
 
-            foreach( $request->condition['lagos']['tag'] as $key => $value) {
+            foreach ($request->condition['lagos']['tag'] as $key => $value) {
                 $shipping_rate = new ShippingRate;
                 $shipping_rate->tag = $request->condition['lagos']['tag'][$key];
                 $shipping_rate->condition = $request->condition['lagos']['condition'][$key];
@@ -350,7 +352,7 @@ class ProductController extends Controller
                 $shipping_rate->save();
             }
 
-            foreach( $request->condition['out_side_lagos']['tag'] as $key => $value) {
+            foreach ($request->condition['out_side_lagos']['tag'] as $key => $value) {
                 $shipping_rate = new ShippingRate;
                 $shipping_rate->tag = $request->condition['out_side_lagos']['tag'][$key];
                 $shipping_rate->condition = $request->condition['out_side_lagos']['condition'][$key];
@@ -366,7 +368,7 @@ class ProductController extends Controller
             $product->product_years()->delete();
         }
 
-        foreach( $request->year_from as $attribute_id => $year) {
+        foreach ($request->year_from as $attribute_id => $year) {
             $product_year = new ProductYear;
             $product_year->attribute_id = $attribute_id;
             $product_year->product_id = $product->id;
@@ -374,7 +376,7 @@ class ProductController extends Controller
             $product_year->save();
         }
 
-        foreach( $request->year_to as $attribute_id => $year) {
+        foreach ($request->year_to as $attribute_id => $year) {
             $product_year = ProductYear::where(['attribute_id' => $attribute_id, 'product_id' => $product->id])->first();
             $product_year->year_to = $year;
             $product_year->save();
@@ -383,14 +385,14 @@ class ProductController extends Controller
 
         $data = json_encode($product->toArray());
 
-        if ( !empty($request->images) ) {
+        if (!empty($request->images)) {
             $images =  $request->images;
-            foreach ( $images as $image) {
+            foreach ($images as $image) {
                 $images = new Image(['image' => $image]);
                 $product->images()->save($images);
             }
         }
-        
+
         //(new Activity)->Log("Added a product ", "{$data}");
         return response()->json($product);
     }
@@ -406,22 +408,22 @@ class ProductController extends Controller
     public function destroy(Request $request)
     {
         //User::canTakeAction(5);
-        $rules = array (
-                '_token' => 'required' 
+        $rules = array(
+            '_token' => 'required'
         );
-        $validator = \Validator::make ( $request->all (), $rules );
-        if (empty ( $request->selected )) {
-            $validator->getMessageBag ()->add ( 'Selected', 'Nothing to Delete' );
-            return \Redirect::back ()->withErrors ( $validator )->withInput ();
+        $validator = \Validator::make($request->all(), $rules);
+        if (empty($request->selected)) {
+            $validator->getMessageBag()->add('Selected', 'Nothing to Delete');
+            return \Redirect::back()->withErrors($validator)->withInput();
         }
         $count = count($request->selected);
-       // (new Activity)->Log("Deleted  {$count} Products");
+        // (new Activity)->Log("Deleted  {$count} Products");
 
-        foreach ( $request->selected as $selected ){
+        foreach ($request->selected as $selected) {
             $delete = Product::find($selected);
             $delete->delete();
         }
-        
+
         return redirect()->back();
     }
 }
--- storage/framework/views/ea6cd13c726f14ef8ad6469875b8f1bf36240a1d.php
+++ storage/framework/views/ea6cd13c726f14ef8ad6469875b8f1bf36240a1d.php
@@ -1,11 +1,75 @@
-<?php $__currentLoopData = $obj->children->sortBy('name'); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $obj): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-    <div class="children" value="<?php echo e($obj->id); ?>">
-       <div class="checkbox">
-            <label>
-                <input type="checkbox" value="<?php echo e($obj->id); ?>" name="selected[]" >
-                <?php echo e($obj->name); ?>  <a href="<?php echo e(route($model.'.edit',[$url => $obj->id])); ?>"><i class="fa fa-pencil"></i> Edit</a>
+<?php if(optional($obj)->children): ?>
+<?php $__currentLoopData = $obj->children->sortBy('name'); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $ob): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+<div class="children" value="<?php echo e($ob->id); ?>">
+    <div class="d-flex">
+        <div class="form-check">
+            <label class="custom-control-label" for="<?php echo e($ob->slug); ?>-<?php echo e($ob->id); ?>">
+                <input class="form-check-input car-models <?php echo e($url); ?> <?php echo e($obj->slug); ?> <?php echo e($obj->name == 'Spare Parts' || $obj->name == 'Servicing Parts'  ? 'no-validation' : ''); ?>" value="<?php echo e($ob->id); ?>" data-slug="<?php echo e($ob->slug); ?>" data-name="<?php echo e($ob->name); ?>" type="checkbox" id="<?php echo e($ob->slug); ?>-<?php echo e($ob->id); ?>" name="<?php echo e($name); ?>[]">
+                <span role="button"><?php echo e($ob->name); ?></span>
+                <a href="<?php echo e(route($route.'.edit',[$url => $ob->id])); ?>">
+                    <i class="fa fa-pencil"></i>
+                    Edit
+                </a>
+
+                <?php if(isset($link)): ?>
+                |
+                <a href="<?php echo e(config('app.url')); ?>/products/<?php echo e($ob->slug); ?>">
+                    <i class="fa fa-external-link" aria-hidden="true"></i>Link
+                </a>
+                <?php endif; ?>
+
+
             </label>
-        </div>  
-    <?php echo $__env->make('includes.children',['obj'=>$obj], \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+        </div>
+
+
+        <?php if(isset($year) && $year): ?>
+
+        <?php if(null !== $ob->attribute_years): ?>
+        <div class="col-sm-3 ml-3 col-12">
+            <div class="input-group input-group-dynamic">
+                <label class="form-label "> </label>
+                <select class="form-control mx-3 year Year_from-<?php echo e($ob->slug); ?>" name="year_from[<?php echo e($ob->id); ?>]" id="">
+                    <option value="">--Year from--</option>
+                    <?php $__currentLoopData = $ob->attribute_years; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $attribute_year): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <option value="<?php echo e($attribute_year->year); ?>"><?php echo e($attribute_year->year); ?> </option>
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                </select>
+            </div>
+        </div>
+        <div class="col-sm-3 ml-3 col-12">
+            <div class="input-group input-group-dynamic">
+                <label class="form-label "> </label>
+                <select class="form-control year Year_to-<?php echo e($ob->slug); ?>" name="year_to[<?php echo e($ob->id); ?>]" id="">
+                    <option value="">--Year to--</option>
+                    <?php $__currentLoopData = $ob->attribute_years; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $attribute_year): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <option class="" value="<?php echo e($attribute_year->year); ?>"><?php echo e($attribute_year->year); ?> </option>
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                </select>
+            </div>
+        </div>
+        <?php endif; ?>
+
+        <?php endif; ?>
+
+
     </div>
-<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?><?php /**PATH /Applications/XAMPP/xamppfiles/htdocs/autofactorng/resources/views/includes/children.blade.php ENDPATH**/ ?>
+
+    <?php if(isset($engine) && $engine && null !== $ob->engines): ?>
+    <?php $__currentLoopData = $ob->engines; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $engine): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+    <div class="children" value="<?php echo e($engine->id); ?>">
+        <div class="d-flex">
+            <div class="form-check">
+                <label class="custom-control-label " for="<?php echo e($ob->slug); ?>-<?php echo e($engine->id); ?>">
+                    <input class="form-check-input  engine-<?php echo e($ob->slug); ?>" value="<?php echo e($engine->id); ?>" type="checkbox" id="<?php echo e($ob->slug); ?>-<?php echo e($engine->id); ?>" name="engine_id[<?php echo e($ob->id); ?>][]" data-slug="<?php echo e($ob->slug); ?>" data-name="<?php echo e($ob->name); ?>">
+                    <span role="button"><?php echo e($engine->name); ?></span>
+                </label>
+            </div>
+        </div>
+    </div>
+    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    <?php endif; ?>
+    <?php echo $__env->make('includes.children',['obj'=>$ob], \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+</div>
+<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+<?php endif; ?><?php /**PATH /Applications/XAMPP/xamppfiles/htdocs/autofactorng/resources/views/includes/children.blade.php ENDPATH**/ ?>
