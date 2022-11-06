50431097efdc031ef151ed6a086173e88e3c8485
Seth Atam <seth.atam@gmail.com>
2022-11-06 02:33:43 +0100

    Updated products

--- app/Http/Controllers/Products/ProductsController.php
+++ app/Http/Controllers/Products/ProductsController.php
@@ -69,7 +69,7 @@ class ProductsController extends Controller
 
         $type = $this->getType($request);
 
-        if (null !== $request->cookie('engine_id')) {
+        if (null !== $request->cookie('engine_id') &&  $request->type !== 'clear') {
             $query->whereHas('make_model_year_engines', function (Builder  $builder) use ($request) {
                 $builder->where('make_model_year_engines.attribute_id', $request->cookie('model_id'));
                 $builder->where('make_model_year_engines.parent_id', $request->cookie('make_id'));
