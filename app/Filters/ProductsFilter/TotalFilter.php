bdc7c1d9e38ad24ccbe34bfd1d122476b1a4ad03
Seth Atam <seth.atam@gmail.com>
2022-11-06 14:41:05 +0100

    Up filter

--- app/Filters/ProductsFilter/TotalFilter.php
+++ app/Filters/ProductsFilter/TotalFilter.php
@@ -9,13 +9,11 @@ use App\Filters\AbstractFilter;
 
 class TotalFilter  extends AbstractFilter
 {
-    public function filter(Builder $builder,$value)
+    public function filter(Builder $builder, $value)
     {
-        if(count($value) == 1){
-            $value  =  array_shift($value);
-            return $builder->where('price','<=' ,$value);
-        }
-        return $builder->whereBetween('price', [min($value),max($value)]);
+
+        $value = explode('-', $value);
+        return  $builder->where('price', '>=', $value[0])
+            ->where('price', '<=', $value[1]);
     }
-    
 }
