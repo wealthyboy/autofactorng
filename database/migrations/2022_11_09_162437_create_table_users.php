5abc906d9301ebc43c80cbed648a47e8cb1255ba
Seth Atam <seth.atam@gmail.com>
2022-11-09 17:27:11 +0100

    Added users

--- database/migrations/2022_11_09_162437_create_table_users.php
+++ database/migrations/2022_11_09_162437_create_table_users.php
@@ -15,6 +15,13 @@ class CreateTableUsers extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('last_name');
+            $table->string('type');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
