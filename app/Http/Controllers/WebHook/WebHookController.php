696b08292cf06c9eb9ab31b424f865db3ae1e483
Seth Atam <seth.atam@gmail.com>
2022-11-12 17:03:29 +0100

    Added users

--- app/Http/Controllers/WebHook/WebHookController.php
+++ app/Http/Controllers/WebHook/WebHookController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\WebHook;
 
 use App\Http\Controllers\Controller;
-// use Illuminate\Http\Request;
+use Illuminate\Http\Request;
 // use Illuminate\Support\Facades\Log;
 // use App\User;
 // use App\Order;
