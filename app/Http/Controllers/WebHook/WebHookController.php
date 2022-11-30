8ad3bf6d5f3d864a53ea52c4065ca9824dae51e6
Seth Atam <seth.atam@gmail.com>
2022-11-30 03:37:29 +0100

    Updated Web

--- app/Http/Controllers/WebHook/WebHookController.php
+++ app/Http/Controllers/WebHook/WebHookController.php
@@ -64,7 +64,7 @@ class WebHookController extends Controller
 
 
                 $admin_emails = explode(',', $this->settings->alert_email);
-                $total =  DB::table('ordered_product')->select(\DB::raw('SUM(ordered_product.price*ordered_product.quantity) as items_total'))->where('order_id', $order->id)->get();
+                $total =  DB::table('ordered_products')->select(DB::raw('SUM(ordered_products.price*ordered_products.quantity) as items_total'))->where('order_id', $order->id)->get();
                 $sub_total = $total[0]->items_total ?? '0.00';
                 $order->currency = '₦';
                 try {
