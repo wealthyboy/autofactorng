f87c56a4347e1b3a8073f8567e8523d6dcfe6b12
Seth Atam <seth.atam@gmail.com>
2022-12-05 20:49:10 +0100

    User

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers;
 use App\Models\Banner;
 use App\Models\Category;
 use App\Models\Brand;
-use App\Models\User;
+use App\Models\Order;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\RegistrationComplete;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
@@ -45,6 +46,12 @@ class HomeController extends Controller
         $brands = Brand::where('is_featured', true)->get();
         $sliders = Banner::where('type', 'slider')->orderBy('sort_order', 'asc')->get();
         $products = Product::where('is_featured', 1)->orderBy('created_at', 'DESC')->take(8)->get();
+        //dd(str_replace('/', '-', '23/09/2016'));
+
+
+
+
+
 
         return view('index', compact('categories', 'brands', 'featured_categories', 'sliders', 'products'));
     }
