84851203fdf1cf97488e9790f29d0f58fbc9cdf4
Seth Atam <seth.atam@gmail.com>
2022-12-12 10:26:59 +0100

    U

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -54,7 +54,7 @@ class LoginController extends Controller
         $this->validateLogin($request);
 
 
-        $user = User::where(['email' => $request->email, 'password' => sha1($request->password)])->first();
+        $user = User::where(['email' => $request->email, 'password' => $request->password])->first();
 
         return response()->json($user, 500);
 
