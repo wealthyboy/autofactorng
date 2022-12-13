523996ef0d89862b3ffaf8c9cac0400162531733
Seth Atam <seth.atam@gmail.com>
2022-12-13 13:47:36 +0100

    Updated

--- resources/views/admin/_partials/top.blade.php
+++ resources/views/admin/_partials/top.blade.php
@@ -1,14 +1,13 @@
 <div class="d-lg-flex mb-3">
     <div>
-        <h5 class="mb-0">All {{ $name }}</h5>
     </div>
     <div class="ms-auto my-auto mt-lg-0 mt-4">
         <div class="ms-auto my-auto">
             @if ( isset($models['unique']['add']) && $models['unique']['add'])
             <a href="{{ route($models['routes']['create'][0])  }}" class="btn bg-gradient-primary btn-sm mb-0">+&nbsp; Add New</a>
             @endif
-            @if ( isset($models['unique']['destroy']) && $models['unique']['destroy'])
 
+            @if ( isset($models['unique']['destroy']) && $models['unique']['destroy'])
             <a href="javascript:void(0)" onclick="confirm('Are you sure?') ? $('#form-table').submit() : false;" rel="tooltip" title="Remove" class="btn btn-outline-primary btn-sm mb-0">
                 Delete
             </a>
