c95bd76f4617837b54cae92fdf1042e4ae5f89d5
Seth Atam <seth.atam@gmail.com>
2022-12-13 13:09:18 +0100

    Updated

--- resources/views/admin/_partials/top.blade.php
+++ resources/views/admin/_partials/top.blade.php
@@ -4,13 +4,13 @@
     </div>
     <div class="ms-auto my-auto mt-lg-0 mt-4">
         <div class="ms-auto my-auto">
-            @if ( isset($add) )
+            @if ( isset($models['unique']['add']) && $models['unique']['add'])
             <a href="{{ route($models['routes']['create'][0])  }}" class="btn bg-gradient-primary btn-sm mb-0">+&nbsp; Add New</a>
             @endif
             <a href="javascript:void(0)" onclick="confirm('Are you sure?') ? $('#form-table').submit() : false;" rel="tooltip" title="Remove" class="btn btn-outline-primary btn-sm mb-0">
                 Delete
             </a>
-            @if ( isset($export) )
+            @if ( isset($models['unique']['export']) && $models['unique']['export'])
             <button class="btn btn-outline-primary btn-sm export mb-0 mt-sm-0 mt-1" data-type="csv" type="button" name="button">Export</button>
             @endif
         </div>
