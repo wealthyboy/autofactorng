28ffe4f867981a82b3846de67846c720ecfd2ee6
Seth Atam <seth.atam@gmail.com>
2022-12-26 10:21:25 +0100

    U

--- storage/framework/views/1580836486459c4dc9db718a2670a7267bd45092.php
+++ storage/framework/views/1580836486459c4dc9db718a2670a7267bd45092.php
@@ -20,19 +20,18 @@
 						}
 					}
 				}">
+
+   <?php $__currentLoopData = $products; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $product): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
    <div class="product-default left-details product-unfold">
       <figure>
-         <a href="product.html">
-            <img src="assets/images/products/product-8.jpg" alt="product" width="300" height="300">
-            <img src="assets/images/products/product-8-2.jpg" alt="product" width="300" height="300">
+         <a href="<?php echo e($product->link); ?>">
+            <img src="<?php echo e($product->image_to_show); ?>" alt="product" width="300" height="300">
+            <img src="<?php echo e($product->image_to_show); ?>" alt="product" width="300" height="300">
          </a>
       </figure>
       <div class="product-details">
-         <div class="category-list">
-            <a href="category.html" class="product-category">clothing</a>,
-            <a href="category.html" class="product-category">shoes</a>
-         </div>
-         <h3 class="product-title"> <a href="product.html">Men Black Glasses</a> </h3>
+
+         <h3 class="product-title"> <a href="<?php echo e($product->link); ?>"><?php echo e($product->name); ?></a> </h3>
          <div class="ratings-container">
             <div class="product-ratings">
                <span class="ratings" style="width:0%"></span>
@@ -43,189 +42,26 @@
          </div>
          <!-- End .product-container -->
          <div class="price-box">
-            <span class="product-price">$99.00 – $109.00</span>
-         </div>
-         <!-- End .price-box -->
-         <div class="product-action">
-            <a href="element-products.html#" class="btn-icon btn-add-cart product-type-simple"><i class="icon-shopping-cart"></i><span>ADD TO CART</span></a>
-            <a href="wishlist.html" class="btn-icon-wish" title="wishlist"><i class="icon-heart"></i></a>
-            <a href="ajax/product-quick-view.html" class="btn-quickview" title="Quick View"><i class="fas fa-external-link-alt"></i></a>
-         </div>
-      </div>
-      <!-- End .product-details -->
-   </div>
-   <div class="product-default left-details product-unfold">
-      <figure>
-         <a href="product.html">
-            <img src="assets/images/products/product-9.jpg" alt="product" width="300" height="300">
-            <img src="assets/images/products/product-9-2.jpg" alt="product" width="300" height="300">
-         </a>
-      </figure>
-      <div class="product-details">
-         <div class="category-list">
-            <a href="category.html" class="product-category">clothing</a>,
-            <a href="category.html" class="product-category">shoes</a>
-         </div>
-         <h3 class="product-title"> <a href="product.html">Black Glasses</a> </h3>
-         <div class="ratings-container">
-            <div class="product-ratings">
-               <span class="ratings" style="width:0%"></span>
-               <!-- End .ratings -->
-               <span class="tooltiptext tooltip-top"></span>
+            <?php if($product->discounted_price): ?>
+            <div>
+               <span class="old-price"><?php echo e($product.currency); ?><?php echo e($product.formatted_sale_price); ?></span>
+               <span class="product-price"><?php echo e($product.currency); ?><?php echo e($product.formatted_price); ?></span>
             </div>
-            <!-- End .product-ratings -->
-         </div>
-         <!-- End .product-container -->
-         <div class="price-box">
-            <span class="product-price">$99.00 – $109.00</span>
-         </div>
-         <!-- End .price-box -->
-         <div class="product-action">
-            <a href="element-products.html#" class="btn-icon btn-add-cart product-type-simple"><i class="icon-shopping-cart"></i><span>ADD TO CART</span></a>
-            <a href="wishlist.html" class="btn-icon-wish" title="wishlist"><i class="icon-heart"></i></a>
-            <a href="ajax/product-quick-view.html" class="btn-quickview" title="Quick View"><i class="fas fa-external-link-alt"></i></a>
-         </div>
-      </div>
-      <!-- End .product-details -->
-   </div>
-   <div class="product-default left-details product-unfold">
-      <figure>
-         <a href="product.html">
-            <img src="assets/images/products/product-10.jpg" alt="product" width="300" height="300">
-            <img src="assets/images/products/product-10-2.jpg" alt="product" width="300" height="300">
-         </a>
-      </figure>
-      <div class="product-details">
-         <div class="category-list">
-            <a href="category.html" class="product-category">clothing</a>,
-            <a href="category.html" class="product-category">shoes</a>
-         </div>
-         <h3 class="product-title"> <a href="product.html">Men Black Shoes</a> </h3>
-         <div class="ratings-container">
-            <div class="product-ratings">
-               <span class="ratings" style="width:0%"></span>
-               <!-- End .ratings -->
-               <span class="tooltiptext tooltip-top"></span>
+            <?php else: ?>
+            <div>
+               <span class="product-price"><?php echo e($product.currency); ?><?php echo e($product.formatted_price); ?></span>
             </div>
-            <!-- End .product-ratings -->
-         </div>
-         <!-- End .product-container -->
-         <div class="price-box">
-            <span class="product-price">$99.00 – $109.00</span>
+            <?php endif; ?>
          </div>
          <!-- End .price-box -->
          <div class="product-action">
-            <a href="product.html" class="btn-icon btn-add-cart"><i class="fa fa-arrow-right"></i><span>SELECT
-                  OPTIONS</span></a>
-            <a href="wishlist.html" class="btn-icon-wish" title="wishlist"><i class="icon-heart"></i></a>
-            <a href="ajax/product-quick-view.html" class="btn-quickview" title="Quick View"><i class="fas fa-external-link-alt"></i></a>
+            <a href="#" class="btn-icon btn-add-cart product-type-simple"><i class="icon-shopping-cart"></i><span>ADD TO CART</span></a>
          </div>
       </div>
       <!-- End .product-details -->
    </div>
-   <div class="product-default left-details product-unfold">
-      <figure>
-         <a href="product.html">
-            <img src="assets/images/products/product-11.jpg" alt="product" width="300" height="300">
-            <img src="assets/images/products/product-11-2.jpg" alt="product" width="300" height="300">
-         </a>
-      </figure>
-      <div class="product-details">
-         <div class="category-list">
-            <a href="category.html" class="product-category">clothing</a>,
-            <a href="category.html" class="product-category">bag</a>
-         </div>
-         <h3 class="product-title"> <a href="product.html">Women Bag</a> </h3>
-         <div class="ratings-container">
-            <div class="product-ratings">
-               <span class="ratings" style="width:0%"></span>
-               <!-- End .ratings -->
-               <span class="tooltiptext tooltip-top"></span>
-            </div>
-            <!-- End .product-ratings -->
-         </div>
-         <!-- End .product-container -->
-         <div class="price-box">
-            <span class="product-price">$99.00 – $109.00</span>
-         </div>
-         <!-- End .price-box -->
-         <div class="product-action">
-            <a href="element-products.html#" class="btn-icon btn-add-cart product-type-simple"><i class="icon-shopping-cart"></i><span>ADD TO CART</span></a>
-            <a href="wishlist.html" class="btn-icon-wish" title="wishlist"><i class="icon-heart"></i></a>
-            <a href="ajax/product-quick-view.html" class="btn-quickview" title="Quick View"><i class="fas fa-external-link-alt"></i></a>
-         </div>
-      </div>
-      <!-- End .product-details -->
-   </div>
-   <div class="product-default left-details product-unfold">
-      <figure>
-         <a href="product.html">
-            <img src="assets/images/products/product-6.jpg" alt="product" width="300" height="300">
-            <img src="assets/images/products/product-6-2.jpg" alt="product" width="300" height="300">
-         </a>
-      </figure>
-      <div class="product-details">
-         <div class="category-list">
-            <a href="category.html" class="product-category">clothing</a>,
-            <a href="category.html" class="product-category">shoes</a>
-         </div>
-         <h3 class="product-title"> <a href="product.html">Brown Belt</a> </h3>
-         <div class="ratings-container">
-            <div class="product-ratings">
-               <span class="ratings" style="width:0%"></span>
-               <!-- End .ratings -->
-               <span class="tooltiptext tooltip-top"></span>
-            </div>
-            <!-- End .product-ratings -->
-         </div>
-         <!-- End .product-container -->
-         <div class="price-box">
-            <span class="product-price">$99.00 – $109.00</span>
-         </div>
-         <!-- End .price-box -->
-         <div class="product-action">
-            <a href="element-products.html#" class="btn-icon btn-add-cart product-type-simple"><i class="icon-shopping-cart"></i><span>ADD TO CART</span></a>
-            <a href="wishlist.html" class="btn-icon-wish" title="wishlist"><i class="icon-heart"></i></a>
-            <a href="ajax/product-quick-view.html" class="btn-quickview" title="Quick View"><i class="fas fa-external-link-alt"></i></a>
-         </div>
-      </div>
-      <!-- End .product-details -->
-   </div>
-   <div class="product-default left-details product-unfold">
-      <figure>
-         <a href="product.html">
-            <img src="assets/images/products/product-5.jpg" alt="product" width="300" height="300">
-            <img src="assets/images/products/product-5-2.jpg" alt="product" width="300" height="300">
-         </a>
-      </figure>
-      <div class="product-details">
-         <div class="category-list">
-            <a href="category.html" class="product-category">clothing</a>,
-            <a href="category.html" class="product-category">shoes</a>
-         </div>
-         <h3 class="product-title"> <a href="product.html">Men Gentle Shoes</a> </h3>
-         <div class="ratings-container">
-            <div class="product-ratings">
-               <span class="ratings" style="width:0%"></span>
-               <!-- End .ratings -->
-               <span class="tooltiptext tooltip-top"></span>
-            </div>
-            <!-- End .product-ratings -->
-         </div>
-         <!-- End .product-container -->
-         <div class="price-box">
-            <span class="product-price">$99.00 – $109.00</span>
-         </div>
-         <!-- End .price-box -->
-         <div class="product-action">
-            <a href="product.html" class="btn-icon btn-add-cart"><i class="fa fa-arrow-right"></i><span>SELECT
-                  OPTIONS</span></a>
-            <a href="wishlist.html" class="btn-icon-wish" title="wishlist"><i class="icon-heart"></i></a>
-            <a href="ajax/product-quick-view.html" class="btn-quickview" title="Quick View"><i class="fas fa-external-link-alt"></i></a>
-         </div>
-      </div>
-      <!-- End .product-details -->
-   </div>
+   <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+
 </div>
 
 <?php endif; ?><?php /**PATH /Applications/XAMPP/xamppfiles/htdocs/autofactorng/resources/views/_partials/recently_viewed_products.blade.php ENDPATH**/ ?>
