c1e37b6bdcb92b62a20d47c7249449df9261d3f9
Seth Atam <seth.atam@gmail.com>
2023-01-03 12:25:53 +0100

    U

--- resources/views/admin/customers/show.blade.php
+++ resources/views/admin/customers/show.blade.php
@@ -26,7 +26,6 @@
 
 
                   <div class="col-3">
-                     <label for="">Choose Status</label>
                      <select name="status" class="form-select  mb-3 border p-2 ps-2" aria-label=".form-select-lg">
                         <option selected value="">Choose Status</option>
                         <option class="" value="added">Added </option>
