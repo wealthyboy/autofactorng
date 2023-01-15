1eb79fe20acab95fd3bf041eb79263d0612b6361
Seth Atam <seth.atam@gmail.com>
2023-01-15 11:08:24 +0100

    U

--- app/Http/Controllers/Api/Address/AddressController.php
+++ app/Http/Controllers/Api/Address/AddressController.php
@@ -63,15 +63,19 @@ class AddressController extends Controller
 
         foreach ($carts as $key => $cart) {
             if ($cart->product->condition_is_present) {
-                $heavy_item_price[] = ShippingRate::where(['product_id' => $cart->product_id, 'is_lagos' => $is_lagos])->where(function (Builder $query) use ($cart) {
-                    $query
-                        ->where('tag_value', '<=', $cart->quantity)
-                        ->where('tag', 'quantity')
-                        ->orWhere('tag_value', '>', $cart->quantity)
-                        ->orWhere('tag_value', '=', $cart->quantity);
-                })
-                    ->select('price')
-                    ->first()->toArray();
+                $heavy_item_prices = ShippingRate::where(['product_id' => $cart->product_id, 'is_lagos' => $is_lagos])
+                    ->get();
+
+
+                foreach ($heavy_item_prices as $heavy_item_price) {
+                    if ($heavy_item_price->condition == '=') {
+                        $heavy_item_prices[] = $cart->quantity == $heavy_item_price->tag_value ? $heavy_item_price->price :  null;
+                    }
+
+                    if ($heavy_item_price->condition == '>') {
+                        $heavy_item_prices[] = $cart->quantity > $heavy_item_price->tag_value ? $heavy_item_price->price :  null;
+                    }
+                }
             }
         }
 
