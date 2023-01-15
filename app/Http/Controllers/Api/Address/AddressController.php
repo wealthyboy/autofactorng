731160b133a9e287fce6ad1a902b137d93f0d4ff
Seth Atam <seth.atam@gmail.com>
2023-01-15 10:26:13 +0100

    U

--- app/Http/Controllers/Api/Address/AddressController.php
+++ app/Http/Controllers/Api/Address/AddressController.php
@@ -61,11 +61,12 @@ class AddressController extends Controller
 
         foreach ($carts as $key => $cart) {
             if ($cart->product->condition_is_present) {
-                $heavy_item_price[] = ShippingRate::where(['product_id' => $cart->product_id, 'is_lagos' => $is_lagos])->where(function ($query) use ($cart) {
-                    $query->where('tag_value', '=', $cart->quantity);
-                })
-                    ->select('price')
-                    ->first()->toArray();
+                // $heavy_item_price[] = ShippingRate::where(['product_id' => $cart->product_id, 'is_lagos' => $is_lagos])->where(function ($query) use ($cart) {
+                //     $query->where('tag_value', '=', $cart->quantity);
+                // })
+                //     ->select('price')
+                //     ->first()->toArray();
+                return true;
             }
         }
 
