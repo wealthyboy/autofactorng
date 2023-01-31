f4d032a85a014f7c0a6db5b810e8b3cea01b8d49
Seth Atam <seth.atam@gmail.com>
2023-01-31 09:00:28 +0100

    U

--- app/Models/BrandCategory.php
+++ app/Models/BrandCategory.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class BrandCategory extends Model
 {
     use HasFactory;
+
+    protected $table = 'brand_category';
+
+
+    protected $fillable =  ['category_id', 'brand_id'];
 }
