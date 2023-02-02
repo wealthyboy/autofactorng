410528d83fc412bd782cf995f84989cf85e7eaf2
Seth Atam <seth.atam@gmail.com>
2023-02-02 14:38:23 +0100

    U

--- app/Http/Controllers/Products/ProductsController.php
+++ app/Http/Controllers/Products/ProductsController.php
@@ -231,13 +231,12 @@ class ProductsController extends Controller
 
             $product = Product::where('slug', $request->product)->first();
 
-            $p = Product::whereHas('make_model_year_engines', function (Builder  $builder) use ($request, $product) {
+            $p = Product::where('id', $product->id)->whereHas('make_model_year_engines', function (Builder  $builder) use ($request, $product) {
                 $builder->where('make_model_year_engines.attribute_id', $request->cookie('model_id'));
                 $builder->where('make_model_year_engines.parent_id', $request->cookie('make_id'));
                 $builder->where('make_model_year_engines.engine_id', $request->cookie('engine_id'));
                 $builder->where('year_from', '<=', $request->cookie('year'));
                 $builder->where('year_to', '>=', $request->cookie('year'));
-                $builder->where('id', $product->id);
                 $builder->groupBy('make_model_year_engines.product_id');
             })->first();
 
