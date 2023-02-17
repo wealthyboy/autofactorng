a31a8b8444b2604130422faed4947e5a90b3f571
Jacob Atam <jacobatam@Jacobs-MacBook-Pro.local>
2023-02-17 00:10:15 -0800

    U

--- resources/views/admin/_partials/t.blade.php
+++ resources/views/admin/_partials/t.blade.php
@@ -103,7 +103,7 @@
 
                                         @else
 
-                                        <h6 id="<?php echo  $k == 'Price' ? $models["items"][0][$key]["Id"] : null ?>" {{ class="mb-0 text-xs" {{ $k == 'Price' ? 'contenteditable' : null }}>{{ $v }}</h6>
+                                        <h6 id="<?php echo  $k == 'Price' ? $models['items'][0][$key]['Id'] : null ?>" class="mb-0 text-xs" {{ $k == 'Price' ? 'contenteditable' : null }}>{{ $v }}</h6>
 
                                         @endif
                                     </select>
