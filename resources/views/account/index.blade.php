4895d66b468269fba259b09dbaf5f15dd883d772
Jacob Atam <jacobatam@Jacobs-MacBook-Pro.local>
2023-02-19 13:16:53 -0800

    U

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -5,7 +5,7 @@
 <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
    <ol class="breadcrumb">
       <li class="breadcrumb-item"><a href="#">Home</a></li>
-      <li class="breadcrumb-item active" aria-current="page">Library</li>
+      <li class="breadcrumb-item active" aria-current="page">Acctoun</li>
    </ol>
 </nav>
 
