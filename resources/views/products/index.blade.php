0aec61eecccfc8cf5a62460241513cf8cfc89323
Jacob Atam <jacobatam@Jacobs-MacBook-Pro.local>
2023-02-19 05:14:26 -0800

    U

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -10,9 +10,8 @@
             <div class="col-12">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item"><a href="#">Library</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Data</li>
+                        <li class="breadcrumb-item"><a href="/">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">{{ $category->name }}</li>
                     </ol>
                 </nav>
             </div>
