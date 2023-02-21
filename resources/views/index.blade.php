d63ef92f679137dfd6103a6c2c75692e94229363
Jacob Atam <jacobatam@Jacobs-MacBook-Pro.local>
2023-02-20 20:50:01 -0800

    U

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,8 +7,6 @@
     <!-- <div class="p-3 mb-2 bg-danger text-white text-center">Just in Autocover now available</div> -->
     <div class="row g-2">
         @include('_partials.slider')
-
-
     </div>
 </div>
 
