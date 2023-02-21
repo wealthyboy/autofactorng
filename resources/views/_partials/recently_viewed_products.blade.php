e6a6d591f9393462445e8a1c260cc3c9bb04eeec
Jacob Atam <jacobatam@Jacobs-MacBook-Pro.local>
2023-02-21 10:59:14 -0800

    U

--- resources/views/_partials/recently_viewed_products.blade.php
+++ resources/views/_partials/recently_viewed_products.blade.php
@@ -40,23 +40,6 @@
             <div class="az_ll az_cm">
                <div class="az_phb az_rhb" data-testid="part-label">Prestone Antifreeze/Coolant Universal 50/50 PREMIXED *10 Year/300K Mile Protection* 1 Gallon</div>
             </div>
-            <div class="az_ll az_cm az_yhb">
-               <div class="az_kl az_sl az_aJb">
-                  <div class="az_ll"><span class="az_eUb" role="img" aria-label="4.9 Stars"><span class="az_fUb az_hUb" aria-hidden="true"><svg class="az_o_b" focusable="false" viewBox="0 0 24 24" aria-hidden="true">
-                              <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
-                           </svg></span><span class="az_fUb az_hUb" aria-hidden="true"><svg class="az_o_b" focusable="false" viewBox="0 0 24 24" aria-hidden="true">
-                              <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
-                           </svg></span><span class="az_fUb az_hUb" aria-hidden="true"><svg class="az_o_b" focusable="false" viewBox="0 0 24 24" aria-hidden="true">
-                              <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
-                           </svg></span><span class="az_fUb az_hUb" aria-hidden="true"><svg class="az_o_b" focusable="false" viewBox="0 0 24 24" aria-hidden="true">
-                              <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
-                           </svg></span><span class="az_fUb az_gUb" aria-hidden="true"><svg class="az_o_b" focusable="false" viewBox="0 0 24 24" aria-hidden="true">
-                              <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
-                           </svg></span></span></div>
-                  <div class="az_ll"><span class="az_ra az_ZIb">4.9</span></div>
-                  <div class="az_ll"><span class="az_ra az__Ib">(931)</span></div>
-               </div>
-            </div>
             <div class="az_ll az_cm">
                <div class="az_kl az_ll az_Hl az_sl az_cm az_thb">
                   <div class="az_z9">
