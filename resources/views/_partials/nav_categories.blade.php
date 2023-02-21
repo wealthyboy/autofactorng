efe5dd775a8fb0aada9b724d3b42de92f8a29f26
Jacob Atam <jacobatam@Jacobs-MacBook-Pro.local>
2023-02-21 11:20:14 -0800

    U

--- resources/views/_partials/nav_categories.blade.php
+++ resources/views/_partials/nav_categories.blade.php
@@ -12,7 +12,7 @@
                 <a href="{{ $category->children->count() ? '#' : '/products/'.$category->slug }}" target="" data-testid="at_popular_part_list_item_0" tabindex="0">
                     <div class="az_ylb">
                         <div class="" tabindex="-1" role="menuitem" aria-disabled="false">
-                            <h2 class="">{{ $category->name }}</h2>
+                            <h2 class="mb-0">{{ $category->name }}</h2>
                         </div>
                     </div>
                 </a>
