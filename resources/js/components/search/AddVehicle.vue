026f2724cb332229952b4fd4262e686fef35e5e6
Jacob Atam <jacobatam@Jacobs-MacBook-Pro.local>
2023-02-20 20:37:47 -0800

    U

--- resources/js/components/search/AddVehicle.vue
+++ resources/js/components/search/AddVehicle.vue
@@ -6,8 +6,10 @@
         class=""
     />
     <template v-if="!fitString">
+        <div class="underline w-100"></div>
+
         <div class="title w-100 p-2 mt-sm-3">
-            <h3>SET YOUR VEHICLE</h3>
+            <h3>WHAT ARE YOU WORKING ON TODAY?</h3>
             <p>Get an exact fit for your vehicle.</p>
         </div>
         <div class="d-flex justify-content-between align-content-center pt-2">
