a7609be5bb6b71d3190f79828cb2e7e5d46dcc2e
Jacob Atam <jacobatam@Jacobs-MacBook-Pro.local>
2023-02-20 19:59:06 -0800

    U

--- resources/views/_partials/brands.blade.php
+++ resources/views/_partials/brands.blade.php
@@ -1,7 +1,7 @@
 @if($featured_categories->count())
 <div class="row mt-5">
-    <h2 class="appear-animate" data-animation-delay="100" data-animation-name="fadeInUpShorter">Featured brands</h2>
-    <div class="underline mb-5"></div>
+    <h2 class="appear-animate mb-0 mb-1" data-animation-delay="100" data-animation-name="fadeInUpShorter">Featured brands</h2>
+    <div class="underline mb-5 ms-3"></div>
 </div>
 <div class="row g-0">
     @foreach($brands as $key => $brand)
