2852cef018b7b2e9faa8f6d4f4f5dbcc5990ad77
Jacob <jacob.atam@gmail.com>
2023-02-28 08:22:44 -0800

    U

--- storage/framework/views/76c9ce350507c8373a9d104e4a6d0d70df876346.php
+++ storage/framework/views/76c9ce350507c8373a9d104e4a6d0d70df876346.php
@@ -42,21 +42,28 @@
 
 
         <div class="position-relative  d-none d-lg-block d-md-block d-xl-block col-12  col-md-7  position-relative bg-gradient-primary h-100  px-7 border-radius-lg" style="background-image: url(&quot;/images/utils/sign.jpeg&quot;); background-size: cover; background-repeat: no-repeat; height: 100vh !important; background-position: center center;     background-position: -450px 0px;">
+
             <div class="image-overlay d-flex align-items-center">
+
                 <div class="text text-white ">
-                    <h1 class="text-white mb-4 me-3"> MEMBER BENEFITS: </h1>
+                    <div class=" ">
+                        At Autofactor, we have an extensive inventory of over 50,000 items to cater to your needs, whether online or at our physical store. Also, you'll have access to a range of exclusive benefits and incentives that are designed to make your shopping experience even better.
+                        Sign up today to enjoy this and get even more great benefits.
+                    </div>
+
+                    <h1 class="text-white mb-4"> MEMBER BENEFITS: </h1>
                     <ul>
                         <li class="right-side-bullets">
                             <div class="right-content-icons"><img class="right-content-svgs" alt="" src="/images/rewards.svg"></div>
-                            <div class="bullet-text"> Earn a $20 Reward after every 5 purchases of $20 or more </div>
+                            <div class="bullet-text">Discounts and offers: Receive exclusive discounts on auto parts and accessories. </div>
                         </li>
                         <li class="right-side-bullets">
                             <div class="right-content-icons"><img class="right-content-svgs" alt="" src="/images/vehicle/orange_1.svg"></div>
-                            <div class="bullet-text"> Save your vehicles, track your service history and access thousands of repair guides, all for free </div>
+                            <div class="bullet-text"> Priority access: You'll be the first to know about new products, limited-time offers, and other special promotions.</div>
                         </li>
                         <li class="right-side-bullets">
                             <div class="right-content-icons"><img class="right-content-svgs" alt="" src="/images/orangeStar.svg"></div>
-                            <div class="bullet-text"> Get exclusive deals and offers, customized for you </div>
+                            <div class="bullet-text"> Personalized product recommendations based on your previous purchases and preferences.</div>
                         </li>
                     </ul>
                 </div>
