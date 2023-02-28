c17e64e871509217a9cea51d59dd9042d3c0f01d
Jacob <jacob.atam@gmail.com>
2023-02-28 05:19:27 -0800

    U

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -45,11 +45,12 @@
 
         <div class="position-relative  d-none d-lg-block d-md-block d-xl-block col-12  col-md-7  position-relative bg-gradient-primary h-100  px-7 border-radius-lg" style="background-image: url(&quot;/images/utils/sign.jpeg&quot;); background-size: cover; background-repeat: no-repeat; height: 100vh !important; background-position: center center;     background-position: -450px 0px;">
             <div class="image-overlay d-flex align-items-center">
-                <div>
-                    At Autofactor, we have an extensive inventory of over 50,000 items to cater to your needs, whether online or at our physical store. Also, you'll have access to a range of exclusive benefits and incentives that are designed to make your shopping experience even better.
-                    Sign up today to enjoy this and get even more great benefits.
-                </div>
+
                 <div class="text text-white ">
+                    <div>
+                        At Autofactor, we have an extensive inventory of over 50,000 items to cater to your needs, whether online or at our physical store. Also, you'll have access to a range of exclusive benefits and incentives that are designed to make your shopping experience even better.
+                        Sign up today to enjoy this and get even more great benefits.
+                    </div>
                     <h1 class="text-white mb-4 me-3"> MEMBER BENEFITS: </h1>
                     <ul>
                         <li class="right-side-bullets">
