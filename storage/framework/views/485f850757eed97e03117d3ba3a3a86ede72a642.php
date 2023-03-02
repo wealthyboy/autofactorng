93602bf357238271d730b5347416cb9002c5de01
Jacob <jacob.atam@gmail.com>
2023-03-02 02:59:12 -0800

    U

--- storage/framework/views/485f850757eed97e03117d3ba3a3a86ede72a642.php
+++ storage/framework/views/485f850757eed97e03117d3ba3a3a86ede72a642.php
@@ -1,21 +1,17 @@
-
 <?php if(null !== $global_promo): ?>
-    <div class="container-fluid text-center ">
-        <div style="background-color: <?php echo e($global_promo->bgcolor); ?>" class="top-notice py-2 text-white mb-2">
-            <div class="row">
-                <div class="offer-hignlight ">
-                    <?php $__currentLoopData = $global_promo->promo_texts; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $promo_text): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-                        <div class="col-12 text-center ">
-                            <div class="d-inline-block  text-sm fw-bold text-white text-uppercase  mb-0"><b>
+<div class="container-fluid text-center mt-3">
+    <div style="background-color: <?php echo e($global_promo->bgcolor); ?>" class="top-notice py-2 text-white mb-2">
+        <div class="row">
+            <div class="offer-hignlight ">
+                <?php $__currentLoopData = $global_promo->promo_texts; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $promo_text): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                <div class="col-12 text-center ">
+                    <div class="d-inline-block  text-sm fw-bold text-white text-uppercase  mb-0"><b>
                             <?php echo e($promo_text->promo); ?></b>
-                            </div>
-                        </div>
-                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                    </div>
                 </div>
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
             </div>
-        </div><!-- End .container -->
-    </div>
-<?php endif; ?>
-
-
-<?php /**PATH /Applications/XAMPP/xamppfiles/htdocs/autofactorng/resources/views/_partials/top_banner.blade.php ENDPATH**/ ?>
+        </div>
+    </div><!-- End .container -->
+</div>
+<?php endif; ?><?php /**PATH /Applications/XAMPP/xamppfiles/htdocs/autofactorng/resources/views/_partials/top_banner.blade.php ENDPATH**/ ?>
