a79c1de10b8aac114a209e69e18c49fdcdce135a
Jacob <jacob.atam@gmail.com>
2023-03-02 02:20:11 -0800

    U

--- resources/views/_partials/top_banner.blade.php
+++ resources/views/_partials/top_banner.blade.php
@@ -1,20 +1,17 @@
-
 @if (null !== $global_promo)
-    <div class="container-fluid text-center ">
-        <div style="background-color: {{ $global_promo->bgcolor }}" class="top-notice py-2 text-white mb-2">
-            <div class="row">
-                <div class="offer-hignlight ">
-                    @foreach($global_promo->promo_texts as $promo_text)
-                        <div class="col-12 text-center ">
-                            <div class="d-inline-block  text-sm fw-bold text-white text-uppercase  mb-0"><b>
+<div class="container-fluid text-center mt-3">
+    <div style="background-color: {{ $global_promo->bgcolor }}" class="top-notice py-2 text-white mb-2">
+        <div class="row">
+            <div class="offer-hignlight ">
+                @foreach($global_promo->promo_texts as $promo_text)
+                <div class="col-12 text-center ">
+                    <div class="d-inline-block  text-sm fw-bold text-white text-uppercase  mb-0"><b>
                             {{ $promo_text->promo}}</b>
-                            </div>
-                        </div>
-                    @endforeach
+                    </div>
                 </div>
+                @endforeach
             </div>
-        </div><!-- End .container -->
-    </div>
+        </div>
+    </div><!-- End .container -->
+</div>
 @endif
-
-
