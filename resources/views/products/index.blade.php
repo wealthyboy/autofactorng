ade8d3a1455a0afba5f8e9e9bd7d16fd5f1bea1e
Jacob <jacob.atam@gmail.com>
2023-03-04 14:20:43 +0100

    U

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -7,7 +7,7 @@
 <section class="">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12">
+            <div class="col-12  mt-3">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="/">Home</a></li>
