6a23fae961549860fc431433eaf731fba3c4a1f1
Jacob <jacob.atam@gmail.com>
2023-03-05 04:44:39 +0100

    U

--- app/Http/Controllers/Admin/Product/ProductController.php
+++ app/Http/Controllers/Admin/Product/ProductController.php
@@ -65,7 +65,7 @@ class ProductController extends Table
     {
 
 
-        $products = Product::offset(400)
+        $products = Product::offset(600)
             ->limit(200)->get();
         foreach ($products as $key => $product) {
             foreach ($product->images as $key => $image) {
