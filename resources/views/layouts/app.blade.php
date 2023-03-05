ac4587cdb593839ad3e3e9df108e0ece22100e7c
Jacob <jacob.atam@gmail.com>
2023-03-05 21:12:31 +0100

    U

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -125,7 +125,7 @@
                               <span class="material-symbols-outlined">
                                  local_library
                               </span>
-                              <div class="align-self-center">How To</div>
+                              <div class="align-self-center fw-bold ms-3 fs-6">How To</div>
                               <div class="align-self-center"><img src="/images/utils/header-arrow.svg" alt=""></div>
                            </div>
                         </a>
