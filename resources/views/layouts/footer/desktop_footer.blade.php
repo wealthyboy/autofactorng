25f2399c6a8bd00a76399aceeb4840b1554c8eda
Jacob <jacob.atam@gmail.com>
2023-03-06 14:27:52 +0100

    U

--- resources/views/layouts/footer/desktop_footer.blade.php
+++ resources/views/layouts/footer/desktop_footer.blade.php
@@ -2,9 +2,9 @@
     @foreach($footer_info as $info)
     <div class="col-sm-4 col-6 col-lg-4">
         <div class="widget">
-            <h2 class="widget-title">{{ title_case($info->title) }}</h2>
+            <h2 class="widget-title text-white">{{ title_case($info->title) }}</h2>
             @if($info->children->count())
-            <ul class="">
+            <ul class="links text-white list-unstyled">
                 @foreach($info->children as $info)
                 <li>
                     <a href="{{ $info->link }}">
@@ -18,5 +18,7 @@
         </div><!-- End .widget -->
     </div><!-- End .col-sm-6 -->
     @endforeach
+
+
 
 </div><!-- End .row -->
