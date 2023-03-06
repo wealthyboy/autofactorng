8979d6c5af032e7ba4f62de7845c0a9e22926304
Jacob <jacob.atam@gmail.com>
2023-03-06 11:14:06 +0100

    U

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+<nav style="--bs-breadcrumb-divider: '>';" class="mt-3" aria-label="breadcrumb">
    <ol class="breadcrumb">
       <li class="breadcrumb-item"><a href="#">Home</a></li>
       <li class="breadcrumb-item active" aria-current="page">Account</li>
