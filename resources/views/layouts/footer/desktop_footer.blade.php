61b06370fbc7f43f63fe2ed670c3a654789e429a
Jacob <jacob.atam@gmail.com>
2023-03-06 14:36:13 +0100

    U

--- resources/views/layouts/footer/desktop_footer.blade.php
+++ resources/views/layouts/footer/desktop_footer.blade.php
@@ -2,13 +2,13 @@
     @foreach($footer_info as $info)
     <div class="col-sm-4 col-6 col-lg-4">
         <div class="widget">
-            <h2 class="widget-title text-white">{{ title_case($info->title) }}</h2>
+            <h2 class="widget-title text-white">{{ title_case($info->name) }}</h2>
             @if($info->children->count())
             <ul class="links text-white list-unstyled">
                 @foreach($info->children as $info)
                 <li>
                     <a href="{{ $info->link }}">
-                        {{ $info->title }}
+                        {{ $info->name }}
                     </a>
                 </li>
                 @endforeach
@@ -19,21 +19,4 @@
     </div><!-- End .col-sm-6 -->
     @endforeach
 
-
-
-    <div class="col-lg-4 col-sm-4">
-        <div class="widget">
-            <h4 class="widget-title text-white">Help</h4>
-
-            <ul class="links list-unstyled">
-                <li><a href="about.html">About </a></li>
-                <li><a href="#">Our Guarantees</a></li>
-                <li><a href="#">Terms And Conditions</a></li>
-                <li><a href="#">Privacy policy</a></li>
-                <li><a href="#">Return Policy</a></li>
-                <li><a href="#">Intellectual Property Claims</a></li>
-                <li><a href="#">Site Map</a></li>
-            </ul>
-        </div><!-- End .widget -->
-    </div><!-- End .col-lg-2 -->
 </div><!-- End .row -->
--- storage/framework/views/7d6786f1d9c3f7e4ea32b057b013d95588990b0e.php
+++ storage/framework/views/7d6786f1d9c3f7e4ea32b057b013d95588990b0e.php
@@ -21,5 +21,23 @@
     <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 
 
+    <?php $__currentLoopData = $footer_info; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $info): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+
+    <div class="col-lg-4 col-sm-4">
+        <div class="widget">
+            <h4 class="widget-title text-white"><?php echo e(title_case($info->name)); ?></h4>
+
+            <ul class="links list-unstyled">
+                <li><a href="about.html">About </a></li>
+                <li><a href="#">Our Guarantees</a></li>
+                <li><a href="#">Terms And Conditions</a></li>
+                <li><a href="#">Privacy policy</a></li>
+                <li><a href="#">Return Policy</a></li>
+                <li><a href="#">Intellectual Property Claims</a></li>
+                <li><a href="#">Site Map</a></li>
+            </ul>
+        </div><!-- End .widget -->
+    </div><!-- End .col-lg-2 -->
+    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 
 </div><!-- End .row --><?php /**PATH /Applications/XAMPP/xamppfiles/htdocs/autofactorng/resources/views/layouts/footer/desktop_footer.blade.php ENDPATH**/ ?>
