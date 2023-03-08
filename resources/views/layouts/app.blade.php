1d6ed9ac97791867563cd7dcda41d653f8320a68
Jacob <jacob.atam@gmail.com>
2023-03-08 16:53:17 +0100

    U

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -407,13 +407,10 @@
 
    <div class="watsapp ">
       <a class="chat-on-watsapp d-flex justify-content-between align-items-center" target="_blank" href="https://wa.me/+2">
-         <span class="d-flex flex-column me-4">
-            <span>Need help?</span>
-            <span>
-               Chat with us
-            </span>
+
+         <span>
+            <img src="/images/utils/afng-whatsapp.png" alt="" width="35" srcset="">
          </span>
-         <i class="fab fa-whatsapp fa-2x  mr-2 text-success"></i>
       </a>
    </div>
 
