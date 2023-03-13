a9564f68d4ebdf81773cf159d9c378ea0814ac4d
Jacob <jacob.atam@gmail.com>
2023-03-13 17:05:20 +0100

    U

--- app/Http/Controllers/WebHook/WebHookController.php
+++ app/Http/Controllers/WebHook/WebHookController.php
@@ -74,6 +74,10 @@ class WebHookController extends Controller
                         ->send(new OrderReceipt($order, null, null, $sub_total));
                 } catch (\Throwable $th) {
                     Log::info("Mail error :" . $th);
+                    Log::info("Custom error :" . $th);
+                    $err = new Error();
+                    $err->error = $th->getMessage();
+                    $err->save();
                 }
 
                 //delete cart
