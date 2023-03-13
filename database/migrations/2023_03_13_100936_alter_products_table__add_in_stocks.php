3a6c653e5b17da13fe7113b135ea82fa5d946fa8
Jacob <jacob.atam@gmail.com>
2023-03-13 13:22:10 +0100

    U

--- database/migrations/2023_03_13_100936_alter_products_table__add_in_stocks.php
+++ database/migrations/2023_03_13_100936_alter_products_table__add_in_stocks.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AlterProductsTableAddInStock extends Migration
+class AlterProductsTableAddInStocks extends Migration
 {
     /**
      * Run the migrations.
