87a8cb22461f6304197df556e92a051d7979dc3e
Jacob <jacob.atam@gmail.com>
2023-03-15 12:23:38 +0100

    Updated checkout

--- resources/views/admin/_partials/t.blade.php
+++ resources/views/admin/_partials/t.blade.php
@@ -92,21 +92,14 @@
                                 <div class="align-middle  text-sm">
 
                                     @if(is_array($v))
-
-
-                                    <select style="width: 100px;" class="form-control mt-3 update_status" name="order_status[25991]" id="">
+                                    <select style="width: 100px;" class="form-control mt-3 update" id="" name="order_status[]" id="">
                                         <option value="0" selected>Choose Status</option>
                                         @foreach($v as $k)
                                         <option value="{{ $k }}">{{ $k }}</option>
-
                                         @endforeach
-
-
                                     </select>
                                     @else
-
                                     <h6 data-price="{{ $k == 'Price' ? $v  : '' }}" data-id="{{ isset( $models['items'][0][$key]['Id']) ?   $models['items'][0][$key]['Id'] : null }}" class=" <?php echo  $k == 'Price' ?  'update_price' : '' ?> mb-0 text-xs" {{ $k == 'Price' ? 'contenteditable' : null }}>{{ $v }}</h6>
-
                                     @endif
                                 </div>
                                 @endif
