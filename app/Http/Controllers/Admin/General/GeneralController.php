0af38dac8bdea052abe28f1a40af93dc0ef5af6e
Jacob <jacob.atam@gmail.com>
2023-03-15 15:45:06 +0100

    Updated checkout

--- app/Http/Controllers/Admin/General/GeneralController.php
+++ app/Http/Controllers/Admin/General/GeneralController.php
@@ -14,7 +14,8 @@ class GeneralController extends Controller
      */
     public function changeStatus(Request $request)
     {
-
+        $c = $request->column;
+        $c = $$c;
         $model = 'App\\Models\\' . $request->model;
         $model = $model::find($request->id);
         $model->{$request->column} = $request->value;
