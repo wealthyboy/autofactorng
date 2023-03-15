c80d6fb846e6967a818abc8f805f8b1d15fc6936
Jacob <jacob.atam@gmail.com>
2023-03-15 14:15:04 +0100

    Updated checkout

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -4,5 +4,8 @@
    @include('admin._partials.t', ['models' => $orders, 'name' => 'Orders'])
 </div>
 @endsection
+@section('page-scripts')
+<script src="{{ asset('backend/products.js') }}"></script>
+@stop
 @section('inline-scripts')
 @stop
