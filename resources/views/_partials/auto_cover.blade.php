bec317ab7107d140cdd2928ff7796e6d7351b67d
Jacob <jacob.atam@gmail.com>
2023-03-16 16:50:02 +0100

    Upades nav

--- resources/views/_partials/auto_cover.blade.php
+++ resources/views/_partials/auto_cover.blade.php
@@ -1,13 +1,13 @@
 <div class="row">
     <div class="col-md-6 ">
-        <a href="/products/spare-parts-cooling-heating-system" class="d-block p-0 ">
+        <a href="/buy-now-pay-later?type=auto_cover" class="d-block p-0 ">
             <div class="align-self-center text-center">
                 <img class="img-fluid" src="/images/utils/BNPL.jpg" alt="" srcset="">
             </div>
         </a>
     </div>
-    <div class="col-md-6 ">
-        <a href="/products/spare-parts-cooling-heating-system" class="d-block p-0 ">
+    <div class="col-md-6 mt">
+        <a href="/plans?type=auto_cover" class="d-block p-0 ">
             <div class="align-self-center text-center">
                 <img class="img-fluid" src="/images/utils/Auto_cover.jpg" alt="" srcset="">
             </div>
