bf84737cfd43c465bdd115d8be969bf5bdbdaa14
Jacob <jacob.atam@gmail.com>
2023-03-16 16:42:17 +0100

    Upades nav

--- resources/views/_partials/auto_cover.blade.php
+++ resources/views/_partials/auto_cover.blade.php
@@ -2,14 +2,14 @@
     <div class="col-md-6">
         <a href="/products/spare-parts-cooling-heating-system" class="d-block p-0 ">
             <div class="align-self-center text-center">
-                <img class="img-fluid" src="https://autofactor.ng/images/banners/dLgmEOYrhOiPA1PCtbo2JyfNRTM1KUYZ1x4bLUQi.jpg" alt="" srcset="">
+                <img class="img-fluid" src="/images/utils/ensure11.jpg" alt="" srcset="">
             </div>
         </a>
     </div>
     <div class="col-md-6">
         <a href="/products/spare-parts-cooling-heating-system" class="d-block p-0 ">
             <div class="align-self-center text-center">
-                <img class="img-fluid" src="https://autofactor.ng/images/banners/iJsOh8nJezSHI4eFJXhoXQIPhuI2PY5rhiaeHn0u.jpg" alt="" srcset="">
+                <img class="img-fluid" src="/images/utils/auto_cover.jpg" alt="" srcset="">
             </div>
         </a>
     </div>
