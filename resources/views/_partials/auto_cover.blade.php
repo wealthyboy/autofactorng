f650d2edad3d7876b9af928cd649e6767de6c939
Jacob <jacob.atam@gmail.com>
2023-03-16 16:35:20 +0100

    Upades nav

--- resources/views/_partials/auto_cover.blade.php
+++ resources/views/_partials/auto_cover.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="col-md-6 my-1">
-        <a href="/products/spare-parts-cooling-heating-system" class="d-block p-0 border  py-5">
+        <a href="/products/spare-parts-cooling-heating-system" class="d-block p-0 ">
             <div class="align-self-center text-center">
                 <img class="img-fluid" src="https://autofactor.ng/images/banners/dLgmEOYrhOiPA1PCtbo2JyfNRTM1KUYZ1x4bLUQi.jpg" alt="" srcset="">
             </div>
