ca3f365b2c100a8a1d2ce3db91a4d6e017c5e795
Jacob <jacob.atam@gmail.com>
2023-03-16 13:25:57 +0100

    Upades nav

--- resources/views/_partials/slider.blade.php
+++ resources/views/_partials/slider.blade.php
@@ -3,9 +3,21 @@
    <div class="banner banner3">
       <a href="" class="d-block">
          <figure>
-            <img class="img-fluid" src="https://autofactor.ng/images/banners/mrznVK8edlQm8m43u91CwBGrOQ4B2nmA34eOUtQU.jpg" style="background:#f6e1e8;min-height:36rem;" alt="banner" />
+            <img class="img-fluid" src="https://autofactor.ng/images/banners/mrznVK8edlQm8m43u91CwBGrOQ4B2nmA34eOUtQU.jpg" style="background:#f6e1e8 alt=" banner" />
          </figure>
       </a>
    </div>
 
+</div>
+
+<div class="col-md-3 ">
+
+
+   <div class="banner banner3">
+      <a href="" class="d-block">
+         <figure>
+            <img class="img-fluid" src="https://autofactor.ng/images/banners/sG75MBeseWamL0dV73rS8HtGfSakkilmmor7MIRX.jpg" style="background:#f6e1e8" alt="banner" />
+         </figure>
+      </a>
+   </div>
 </div>
