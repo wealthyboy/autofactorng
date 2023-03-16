66158d827577d5f257805ac66fac77fe4c9c2caa
Jacob <jacob.atam@gmail.com>
2023-03-16 13:40:20 +0100

    Upades nav

--- resources/views/_partials/slider.blade.php
+++ resources/views/_partials/slider.blade.php
@@ -1,18 +1,26 @@
 <div class="col-md-9 ">
+   <section class="p-0">
 
-   <div class="banner banner3">
-      <a href="" class="d-block">
-         <figure>
-            <img class="img-fluid" src="https://autofactor.ng/images/banners/mrznVK8edlQm8m43u91CwBGrOQ4B2nmA34eOUtQU.jpg" style="background:#f6e1e8;" alt=" banner" />
-         </figure>
-      </a>
-   </div>
+      <div class="owl-carousel owl-theme show-nav-hover slide-animate" data-owl-options="{
+           'dots': true,
+           'nav': true,
+           'loop': false
+        }">
+         @foreach($sliders as $key => $slider)
 
+         <div class="banner banner3">
+            <a href="" class="d-block">
+               <figure>
+                  <img width="1920" height="700" src="{{ $slider->image }}" style="background:#f6e1e8;min-height:36rem;" alt="banner" />
+               </figure>
+            </a>
+         </div>
+         @endforeach
+      </div>
+   </section>
 </div>
 
 <div class="col-md-3 ">
-
-
    <div class="banner banner3">
       <a href="" class="d-block">
          <figure>
