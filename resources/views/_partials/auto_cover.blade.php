894ad3107d0c07f519394754569398f39293ab82
Jacob <jacob.atam@gmail.com>
2023-03-16 16:45:26 +0100

    Upades nav

--- resources/views/_partials/auto_cover.blade.php
+++ resources/views/_partials/auto_cover.blade.php
@@ -2,7 +2,7 @@
     <div class="col-md-6">
         <a href="/products/spare-parts-cooling-heating-system" class="d-block p-0 ">
             <div class="align-self-center text-center">
-                <img class="img-fluid" src="/images/utils/Auto_cover.jpg" alt="" srcset="">
+                <img class="img-fluid" src="/images/utils/BNPL.jpg" alt="" srcset="">
             </div>
         </a>
     </div>
