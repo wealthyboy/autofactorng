428acbef216e9020ee3c246c6a48a838cc39c038
Jacob <jacob.atam@gmail.com>
2023-03-16 17:23:13 +0100

    Upades nav

--- resources/views/_partials/auto_cover.blade.php
+++ resources/views/_partials/auto_cover.blade.php
@@ -1,11 +1,4 @@
 <div class="row">
-    <div class="col-md-6 ">
-        <a href="/buy-now-pay-later?type=auto_cover" class="d-block p-0 ">
-            <div class="align-self-center text-center">
-                <img class="img-fluid" src="/images/utils/BNPL.jpg" alt="" srcset="">
-            </div>
-        </a>
-    </div>
     <div class="col-md-6 mt">
         <a href="/plans?type=auto_cover" class="d-block p-0 ">
             <div class="align-self-center text-center">
@@ -13,5 +6,13 @@
             </div>
         </a>
     </div>
+    <div class="col-md-6 ">
+        <a href="/buy-now-pay-later?type=auto_cover" class="d-block p-0 ">
+            <div class="align-self-center text-center">
+                <img class="img-fluid" src="/images/utils/BNPL.jpg" alt="" srcset="">
+            </div>
+        </a>
+    </div>
+
 
 </div>
