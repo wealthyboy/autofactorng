f2ae0a8c3678f812cd958449e9b1b7009bb77847
Jacob <jacob.atam@gmail.com>
2023-03-19 11:41:43 +0100

    Added css

--- resources/views/plans/index.blade.php
+++ resources/views/plans/index.blade.php
@@ -17,6 +17,188 @@
       </div>
     </div>
 
+    <section class="py-3">
+      <div class="bg-gradient-primary">
+        <div class="container pb-lg-9 pb-7 pt-7 postion-relative z-index-2">
+          <div class="row">
+            <div class="col-md-8 mx-auto text-center">
+              <span class="badge bg-gradient-dark mb-2">Pricing</span>
+              <h3 class="text-white">See our pricing</h3>
+              <p class="text-white">You have Free Unlimited Updates and Premium Support on each package.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="mt-n8">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-3 mb-lg-auto mb-4 my-auto p-md-0 ms-auto">
+              <div class="card border-radius-top-end-lg-0 border-radius-bottom-end-lg-0">
+                <div class="card-header text-center pt-4 pb-3">
+                  <h6 class="text-dark opacity-8 mb-0">Starter</h6>
+                  <h1 class="font-weight-bolder">
+                    <small>$</small>199
+                  </h1>
+                </div>
+                <div class="card-body mx-auto pt-0">
+                  <div class="justify-content-start d-flex px-2 py-1">
+                    <div>
+                      <i class="fas fa-check text-dark opacity-6 text-sm"></i>
+                    </div>
+                    <div class="ps-2">
+                      <span class="text-sm">Complete documentation</span>
+                    </div>
+                  </div>
+
+                  <div class="justify-content-start d-flex px-2 py-1">
+                    <div>
+                      <i class="fas fa-check text-dark opacity-6 text-sm"></i>
+                    </div>
+                    <div class="ps-2">
+                      <span class="text-sm">Working materials in Sketch</span>
+                    </div>
+                  </div>
+
+                  <div class="justify-content-start d-flex px-2 py-1">
+                    <div>
+                      <i class="fas fa-check text-dark opacity-6 text-sm"></i>
+                    </div>
+                    <div class="ps-2">
+                      <span class="text-sm">500MB cloud storage</span>
+                    </div>
+                  </div>
+
+                  <div class="justify-content-start d-flex px-2 py-1">
+                    <div>
+                      <i class="fas fa-check text-dark opacity-6 text-sm"></i>
+                    </div>
+                    <div class="ps-2">
+                      <span class="text-sm">Lite support</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="card-footer pt-0">
+                  <a href="javascript:;" class="btn w-100 bg-gradient-dark mb-0">
+                    Buy now
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-3 p-md-0 mb-lg-auto mb-4 z-index-2">
+              <div class="card">
+                <div class="card-header text-center pt-4 pb-3">
+                  <h6 class="text-dark opacity-8 text mb-0 mt-2">Pro</h6>
+                  <h1 class="font-weight-bolder">
+                    <small>$</small>299
+                  </h1>
+                </div>
+                <div class="card-body mx-auto pt-0">
+                  <div class="justify-content-start d-flex px-2 py-1">
+                    <div>
+                      <i class="fas fa-check text-dark opacity-6 text-sm"></i>
+                    </div>
+                    <div class="ps-2">
+                      <span class="text-sm">Complete documentation</span>
+                    </div>
+                  </div>
+
+                  <div class="justify-content-start d-flex px-2 py-1">
+                    <div>
+                      <i class="fas fa-check text-dark opacity-6 text-sm"></i>
+                    </div>
+                    <div class="ps-2">
+                      <span class="text-sm">Working materials in Sketch</span>
+                    </div>
+                  </div>
+
+                  <div class="justify-content-start d-flex px-2 py-1">
+                    <div>
+                      <i class="fas fa-check text-dark opacity-6 text-sm"></i>
+                    </div>
+                    <div class="ps-2">
+                      <span class="text-sm">100GB cloud storage</span>
+                    </div>
+                  </div>
+
+                  <div class="justify-content-start d-flex px-2 py-1">
+                    <div>
+                      <i class="fas fa-check text-dark opacity-6 text-sm"></i>
+                    </div>
+                    <div class="ps-2">
+                      <span class="text-sm">500 team members</span>
+                    </div>
+                  </div>
+
+                  <div class="justify-content-start d-flex px-2 py-1">
+                    <div>
+                      <i class="fas fa-check text-dark opacity-6 text-sm"></i>
+                    </div>
+                    <div class="ps-2">
+                      <span class="text-sm">Premium support</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="card-footer pt-0">
+                  <a href="javascript:;" class="btn btn-icon bg-gradient-primary d-block mb-0 icon-move-right">Try Pro
+                    <i class="fas fa-arrow-right ms-2" aria-hidden="true"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-3 mb-lg-auto mb-4 my-auto p-md-0 me-auto">
+              <div class="card border-radius-top-start-lg-0 border-radius-bottom-start-lg-0">
+                <div class="card-header text-center pt-4 pb-2">
+                  <h6 class="text-dark opacity-8 text mb-0">Premium</h6>
+                  <h1 class="font-weight-bolder">
+                    <small>$</small>399
+                  </h1>
+                </div>
+                <div class="card-body mx-auto pt-0">
+                  <div class="justify-content-start d-flex px-2 py-1 py-1">
+                    <div>
+                      <i class="fas fa-check text-dark opacity-6 text-sm"></i>
+                    </div>
+                    <div class="ps-2">
+                      <span class="text-sm">Complete documentation</span>
+                    </div>
+                  </div>
+                  <div class="justify-content-start d-flex px-2 py-1 py-1">
+                    <div>
+                      <i class="fas fa-check text-dark opacity-6 text-sm"></i>
+                    </div>
+                    <div class="ps-2">
+                      <span class="text-sm">Working materials in Sketch</span>
+                    </div>
+                  </div>
+                  <div class="justify-content-start d-flex px-2 py-1 py-1">
+                    <div>
+                      <i class="fas fa-check text-dark opacity-6 text-sm"></i>
+                    </div>
+                    <div class="ps-2">
+                      <span class="text-sm">2GB cloud storage</span>
+                    </div>
+                  </div>
+                  <div class="justify-content-start d-flex px-2 py-1">
+                    <div>
+                      <i class="fas fa-check text-dark opacity-6 text-sm"></i>
+                    </div>
+                    <div class="ps-2">
+                      <span class="text-sm">Premium support</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="card-footer pt-0">
+                  <a href="javascript:;" class="btn w-100 bg-gradient-dark mb-0">Buy now</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
     <div class="row mt-5">
       @foreach($plans as $key => $plan)
       <div class="col-lg-4 col-sm-6 mb-lg-0 mb-4">
@@ -45,6 +227,8 @@
         </div>
       </div>
       @endforeach
+
+
 
 
 
--- storage/framework/views/d5ae91e07d1898a87065237dd832a994c38b50a3.php
+++ storage/framework/views/d5ae91e07d1898a87065237dd832a994c38b50a3.php
@@ -5,7 +5,7 @@
     <div class="row">
       <div class="col-md12">
         <p>"Whether you're an individual or a business, mitigating risk, enhancing your driving experience, and having confidence in the quality of your replacement parts are crucial. </p>
-        <p> By subscribing to a plan, you'll get funded with purchase credits that you can use to shop on our website, with exclusive benefits and discounts that must be used within one year from the time of purchase. </p>
+        <p> By subscribing to a plan, you'll get funded with purchase credits that you can use to shop on our website, exclusive benefits and discounts that must be used within one year from the time of purchase. </p>
         <p>Don't wait too long to take advantage of the subscription plan benefits!</p>
 
       </div>
@@ -14,6 +14,188 @@
         <p></p>
       </div>
     </div>
+
+    <section class="py-3">
+      <div class="bg-gradient-primary">
+        <div class="container pb-lg-9 pb-7 pt-7 postion-relative z-index-2">
+          <div class="row">
+            <div class="col-md-8 mx-auto text-center">
+              <span class="badge bg-gradient-dark mb-2">Pricing</span>
+              <h3 class="text-white">See our pricing</h3>
+              <p class="text-white">You have Free Unlimited Updates and Premium Support on each package.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="mt-n8">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-3 mb-lg-auto mb-4 my-auto p-md-0 ms-auto">
+              <div class="card border-radius-top-end-lg-0 border-radius-bottom-end-lg-0">
+                <div class="card-header text-center pt-4 pb-3">
+                  <h6 class="text-dark opacity-8 mb-0">Starter</h6>
+                  <h1 class="font-weight-bolder">
+                    <small>$</small>199
+                  </h1>
+                </div>
+                <div class="card-body mx-auto pt-0">
+                  <div class="justify-content-start d-flex px-2 py-1">
+                    <div>
+                      <i class="fas fa-check text-dark opacity-6 text-sm"></i>
+                    </div>
+                    <div class="ps-2">
+                      <span class="text-sm">Complete documentation</span>
+                    </div>
+                  </div>
+
+                  <div class="justify-content-start d-flex px-2 py-1">
+                    <div>
+                      <i class="fas fa-check text-dark opacity-6 text-sm"></i>
+                    </div>
+                    <div class="ps-2">
+                      <span class="text-sm">Working materials in Sketch</span>
+                    </div>
+                  </div>
+
+                  <div class="justify-content-start d-flex px-2 py-1">
+                    <div>
+                      <i class="fas fa-check text-dark opacity-6 text-sm"></i>
+                    </div>
+                    <div class="ps-2">
+                      <span class="text-sm">500MB cloud storage</span>
+                    </div>
+                  </div>
+
+                  <div class="justify-content-start d-flex px-2 py-1">
+                    <div>
+                      <i class="fas fa-check text-dark opacity-6 text-sm"></i>
+                    </div>
+                    <div class="ps-2">
+                      <span class="text-sm">Lite support</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="card-footer pt-0">
+                  <a href="javascript:;" class="btn w-100 bg-gradient-dark mb-0">
+                    Buy now
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-3 p-md-0 mb-lg-auto mb-4 z-index-2">
+              <div class="card">
+                <div class="card-header text-center pt-4 pb-3">
+                  <h6 class="text-dark opacity-8 text mb-0 mt-2">Pro</h6>
+                  <h1 class="font-weight-bolder">
+                    <small>$</small>299
+                  </h1>
+                </div>
+                <div class="card-body mx-auto pt-0">
+                  <div class="justify-content-start d-flex px-2 py-1">
+                    <div>
+                      <i class="fas fa-check text-dark opacity-6 text-sm"></i>
+                    </div>
+                    <div class="ps-2">
+                      <span class="text-sm">Complete documentation</span>
+                    </div>
+                  </div>
+
+                  <div class="justify-content-start d-flex px-2 py-1">
+                    <div>
+                      <i class="fas fa-check text-dark opacity-6 text-sm"></i>
+                    </div>
+                    <div class="ps-2">
+                      <span class="text-sm">Working materials in Sketch</span>
+                    </div>
+                  </div>
+
+                  <div class="justify-content-start d-flex px-2 py-1">
+                    <div>
+                      <i class="fas fa-check text-dark opacity-6 text-sm"></i>
+                    </div>
+                    <div class="ps-2">
+                      <span class="text-sm">100GB cloud storage</span>
+                    </div>
+                  </div>
+
+                  <div class="justify-content-start d-flex px-2 py-1">
+                    <div>
+                      <i class="fas fa-check text-dark opacity-6 text-sm"></i>
+                    </div>
+                    <div class="ps-2">
+                      <span class="text-sm">500 team members</span>
+                    </div>
+                  </div>
+
+                  <div class="justify-content-start d-flex px-2 py-1">
+                    <div>
+                      <i class="fas fa-check text-dark opacity-6 text-sm"></i>
+                    </div>
+                    <div class="ps-2">
+                      <span class="text-sm">Premium support</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="card-footer pt-0">
+                  <a href="javascript:;" class="btn btn-icon bg-gradient-primary d-block mb-0 icon-move-right">Try Pro
+                    <i class="fas fa-arrow-right ms-2" aria-hidden="true"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-3 mb-lg-auto mb-4 my-auto p-md-0 me-auto">
+              <div class="card border-radius-top-start-lg-0 border-radius-bottom-start-lg-0">
+                <div class="card-header text-center pt-4 pb-2">
+                  <h6 class="text-dark opacity-8 text mb-0">Premium</h6>
+                  <h1 class="font-weight-bolder">
+                    <small>$</small>399
+                  </h1>
+                </div>
+                <div class="card-body mx-auto pt-0">
+                  <div class="justify-content-start d-flex px-2 py-1 py-1">
+                    <div>
+                      <i class="fas fa-check text-dark opacity-6 text-sm"></i>
+                    </div>
+                    <div class="ps-2">
+                      <span class="text-sm">Complete documentation</span>
+                    </div>
+                  </div>
+                  <div class="justify-content-start d-flex px-2 py-1 py-1">
+                    <div>
+                      <i class="fas fa-check text-dark opacity-6 text-sm"></i>
+                    </div>
+                    <div class="ps-2">
+                      <span class="text-sm">Working materials in Sketch</span>
+                    </div>
+                  </div>
+                  <div class="justify-content-start d-flex px-2 py-1 py-1">
+                    <div>
+                      <i class="fas fa-check text-dark opacity-6 text-sm"></i>
+                    </div>
+                    <div class="ps-2">
+                      <span class="text-sm">2GB cloud storage</span>
+                    </div>
+                  </div>
+                  <div class="justify-content-start d-flex px-2 py-1">
+                    <div>
+                      <i class="fas fa-check text-dark opacity-6 text-sm"></i>
+                    </div>
+                    <div class="ps-2">
+                      <span class="text-sm">Premium support</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="card-footer pt-0">
+                  <a href="javascript:;" class="btn w-100 bg-gradient-dark mb-0">Buy now</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
 
     <div class="row mt-5">
       <?php $__currentLoopData = $plans; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $key => $plan): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
@@ -48,16 +230,18 @@
 
 
 
+
+
       <div class="mt-5">
 
         <h3>
           HOW IT WORKS
         </h3>
 
-        <p> Choose a Plan: Select your preferred subscription plan and follow the prompts to make your payment.</p>
-        <p> Get Funded: Once your payment is confirmed, the equivalent amount of Autofactor purchase credit will be funded to your account. You can then use this credit to make purchases on our website.</p>
-        <p> Enjoy Your Benefits: All subscription plans are valid for one year from the time of purchase, and any credits or benefits given must be used within this period. During checkout, your available credit will be applied automatically to your purchase.</p>
-        <p> Use It or Lose It: It's important to note that any unused credits or benefits after the expiry of the validity period cannot be rolled over or transferred. So, make sure to take advantage of all your benefits before the end of the validity period.</p>
+        <p> 1. Choose a Plan: Select your preferred subscription plan and follow the prompts to make your payment.</p>
+        <p> 2. Get Funded: Once your payment is confirmed, the equivalent amount of Autofactor purchase credit will be funded to your account. You can then use this credit to make purchases on our website.</p>
+        <p> 3. Enjoy Your Benefits: All subscription plans are valid for one year from the time of purchase, and any credits or benefits given must be used within this period. During checkout, your available credit will be applied automatically to your purchase.</p>
+        <p> 4. Use It or Lose It: It's important to note that any unused credits or benefits after the expiry of the validity period cannot be rolled over or transferred. So, make sure to take advantage of all your benefits before the end of the validity period.</p>
 
 
       </div>
