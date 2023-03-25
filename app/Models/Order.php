79cf9935e60e57a1dee7fa54e2b1e981c818a69b
Jacob <jacob.atam@gmail.com>
2023-03-25 08:07:40 +0100

    Updatd M

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -55,7 +55,7 @@ class Order extends Model
 		$order->first_name = optional($user->active_address)->first_name;
 		$order->last_name = optional($user->active_address)->last_name;
 		$order->email = $user->email;
-		$order->tracking = time();
+		$order->tracking =  rand(100000, time());
 		$order->order_type = "Online";
 		$order->phone_number = $user->phone_number;
 		$order->address = optional($user->active_address)->address;
