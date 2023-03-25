e2632dcc84ced113e9bacf2710b0b401c4f14c56
Jacob <jacob.atam@gmail.com>
2023-03-25 15:49:58 +0100

    Updatd M

--- app/Http/Controllers/Admin/Orders/OrdersController.php
+++ app/Http/Controllers/Admin/Orders/OrdersController.php
@@ -42,6 +42,8 @@ class OrdersController extends Table
 	{
 		$orders = Order::has('ordered_products')->orderBy('created_at', 'desc')->paginate(150);
 		$orders = $this->getColumnListings(request(), $orders);
+
+		dd($orders);
 		return view('admin.orders.index', compact('orders'));
 	}
 
