c353b9174e8f738e30606373cf1a064ba219b0b3
Jacob <jacob.atam@gmail.com>
2023-03-30 23:12:06 +0100

    Updated Slider

--- app/Http/Controllers/Orders/OrdersController.php
+++ app/Http/Controllers/Orders/OrdersController.php
@@ -59,9 +59,16 @@ class OrdersController extends Table
         $total = $order->ordered_products[0]->sum_items($order->id)->items_total;
         $coupon = null;
         if ($order->coupon) {
-            $coupon = optional($order->voucher())->amount . ' % Discount -₦' . number_format((optional($order->voucher())->amount / 100) * $total);
+            $coupon = optional($order->voucher())->amount . '% Discount    -₦' . number_format((optional($order->voucher())->amount / 100) * $total);
         }
-        // $currency =  Helper::getCurrency();
+
+        if ($order->discount) {
+            if ($order->percentage_type == 'percentage') {
+                $coupon = $order->discount . ' % Discount -₦' . number_format(($order->discount / 100) * $total);
+            } else {
+                $coupon = ' Discount' . number_format($order->discount);
+            }
+        }
         return view('orders.show', compact('coupon', 'nav', 'order', 'total', 'page_title'));
     }
 
