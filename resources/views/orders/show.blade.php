72a148f12cc1971da3e4392bb435dbaca424e5ce
Jacob <jacob.atam@gmail.com>
2023-03-30 23:33:55 +0100

    Updated Slider

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -35,7 +35,7 @@
                                 <h3>Cart Total</h3>
                                 <span><span class="bold" id="subtotal">Subtotal:</span> ₦{{ $total }}</span></span> </br>
                                 <span><span class="bold" id="subtotal">Shipping:</span> ₦{{ $order->ship_price }}</span></span> </br>
-                                <span><span class="bold" id="subtotal">Coupon:</span> {{ $order->coupon ?  $coupon : '---' }}</span></span> </br>
+                                <span><span class="bold" id="subtotal">Coupon:</span> {{ $coupon ?? '---' }}</span></span> </br>
 
                                 <span><span class="bold" id="total">Total:</span> ₦{{ $order->get_total() }} </span>
                             </div>
