3192f29c811be11dc2834c3848fb7e33969d3a5f
Jacob <jacob.atam@gmail.com>
2023-03-31 12:41:23 +0100

    Updated Slider

--- resources/views/_partials/mobile_nav.blade.php
+++ resources/views/_partials/mobile_nav.blade.php
@@ -3,7 +3,7 @@
         <div class="col-12 w-100 p-0">
             <div class="dropdown mt-3 mobile-nav">
                 <button class="btn btn-secondary dropdown-toggle d-flex align-items-center" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                    <span class="material-symbols-outlined me-2">
+                    <span class="material-symbols-outlined  me-2">
                         settings
                     </span>
                     <span>Menu</span>
@@ -13,8 +13,8 @@
                     @foreach($nav as $key => $n)
                     <li class="py-3">
                         <a href="{{ $n['link'] }}" class="list-group-item list-group-item-action d-flex align-items-center">
-                            <i class="{{ $n['icon'] }}">{{ $n['iconText'] }}</i>
-                            <span class="ms-2">{{ $key }}</span>
+                            <i class="fs-1  {{ $n['icon'] }}">{{ $n['iconText'] }}</i>
+                            <span class="ms-2 fs-3">{{ $key }}</span>
                         </a>
                     </li>
                     @endforeach
