486008a6b30aae1c0bc5d28a7647cca3008f0635
Jacob <jacob.atam@gmail.com>
2023-03-31 18:23:42 +0100

    Updated Slider

--- resources/views/_partials/slider.blade.php
+++ resources/views/_partials/slider.blade.php
@@ -9,7 +9,7 @@
          @foreach($sliders as $key => $slider)
 
          <div class="banner banner3 {{ $slider->device }}">
-            <a href="{{ $banner->link }}" class="d-block">
+            <a href="{{ $slider->link }}" class="d-block">
                <figure>
                   <img width="1920" height="700" src="{{ $slider->image }}" style="background:#f6e1e8;min-height:36rem;" alt="banner" />
                </figure>
@@ -27,7 +27,7 @@
          @foreach($mobile_sliders as $key => $slider)
 
          <div class="banner banner3 {{ $slider->device }}">
-            <a href="" class="d-block">
+            <a href="{{ $slider->link }}" class="d-block">
                <figure>
                   <img width="1920" height="700" src="{{ $slider->image }}" style="background:#f6e1e8;min-height:36rem;" alt="banner" />
                </figure>
