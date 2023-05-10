c91878ed0c6d68f04d28216dd20d84343ab06a22
Jacob Atam <jacob.atam@gmail.com>
2023-05-10 13:21:36 +0100

    Fixed heavy itm cherge

--- app/Http/Controllers/Admin/Orders/OrdersController.php
+++ app/Http/Controllers/Admin/Orders/OrdersController.php
@@ -181,6 +181,8 @@ class OrdersController extends Table
 				$when = now()->addMinutes(5);
 				Mail::to($user->email)
 					->bcc('damilola@autofactorng.com')
+					->bcc('jacob.atam@gmail.com')
+
 					->send(new OrderReceipt($order, null, null, $sub_total));
 			} catch (\Throwable $th) {
 				Log::info("Mail error :" . $th);
