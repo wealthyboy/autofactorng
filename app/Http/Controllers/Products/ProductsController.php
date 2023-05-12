67bbe0e25614d57aced80340a71ce0e3f54fe651
Jacob Atam <jacob.atam@gmail.com>
2023-05-12 13:33:20 +0100

    updated email

--- app/Http/Controllers/Products/ProductsController.php
+++ app/Http/Controllers/Products/ProductsController.php
@@ -93,16 +93,16 @@ class ProductsController extends Controller
         $category = optional(optional(optional($product)->first())->categories)->first();
 
         if (null !==  $category && $this->getCategory($category)) {
-            // if (null !== $request->cookie('engine_id') &&  $request->type !== 'clear') {
-            //     $query->whereHas('make_model_year_engines', function (Builder  $builder) use ($request) {
-            //         $builder->where('make_model_year_engines.attribute_id', $request->cookie('model_id'));
-            //         $builder->where('make_model_year_engines.parent_id', $request->cookie('make_id'));
-            //         $builder->where('make_model_year_engines.engine_id', $request->cookie('engine_id'));
-            //         $builder->where('year_from', '<=', $request->cookie('year'));
-            //         $builder->where('year_to', '>=', $request->cookie('year'));
-            //         $builder->groupBy('make_model_year_engines.product_id');
-            //     });
-            // }
+            if (null !== $request->cookie('engine_id') &&  $request->type !== 'clear') {
+                $query->whereHas('make_model_year_engines', function (Builder  $builder) use ($request) {
+                    $builder->where('make_model_year_engines.attribute_id', $request->cookie('model_id'));
+                    $builder->where('make_model_year_engines.parent_id', $request->cookie('make_id'));
+                    $builder->where('make_model_year_engines.engine_id', $request->cookie('engine_id'));
+                    $builder->where('year_from', '<=', $request->cookie('year'));
+                    $builder->where('year_to', '>=', $request->cookie('year'));
+                    $builder->groupBy('make_model_year_engines.product_id');
+                });
+            }
         }
 
 
