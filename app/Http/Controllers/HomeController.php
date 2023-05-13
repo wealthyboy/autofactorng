d5f30bc0e0d10192faaf49549cca56dfcaee7af8
Jacob Atam <jacob.atam@gmail.com>
2023-05-13 10:11:42 +0100

    U

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -43,7 +43,7 @@ class HomeController extends Controller
 
         $brands = Brand::where('is_featured', true)->get();
         $sliders = Banner::where(['type' => 'slider', 'device' => 'd-none d-lg-block d-xl-block'])->orderBy('sort_order', 'asc')->get();
-        $mobile_sliders = Banner::where(['type' => 'slider', 'device' => 'd-block d-sm-none'])->orderBy('sort_order', 'asc')->get();
+        $mobile_sliders = Banner::where(['type' => 'slider', 'device' => 'd-lg-none d-sm-block d-md-block'])->orderBy('sort_order', 'asc')->get();
         $products = Product::where('is_featured', 1)->orderBy('created_at', 'DESC')->take(8)->get();
         return view('index', compact('categories', 'brands', 'featured_categories', 'sliders',  'mobile_sliders', 'products'));
     }
