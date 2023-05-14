17b95a062ecdedf5623f573b8938fa37745cf979
Jacob Atam <jacob.atam@gmail.com>
2023-05-14 20:32:28 +0100

    U

--- resources/views/buy_now/index.blade.php
+++ resources/views/buy_now/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.checkout')
 
 @section('content')
-<section style="height: 800px;">
+<section style="height: 700px;">
    <div   class="container   full-bg">
       <div class="row">
          <div  class="col-xl-12 col-lg-12 py-4 ">
