2c1467e78d3531312b50c7be8de5e802ee39ab89
Jacob Atam <jacob.atam@gmail.com>
2023-05-14 21:00:17 +0100

    U

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -243,8 +243,11 @@
                   <a href="/how-to" role="button" class="w-100  ">
                      <div class="d-flex add-a-vehicle justify-content-evenly align-items-center">
                      
-                        <span class="material-icons-outlined">
-videocam
+                        <span class="">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+  <path d="M3 4v16l18-8-18-8z"/>
+</svg>
+
 </span>
                         <div class="align-self-center fw-bold ">Video Tips</div>
 
