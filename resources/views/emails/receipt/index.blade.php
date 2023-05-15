6a40918c98a3ac1b89688365eb4b7029a86ef6fc
Jacob Atam <jacob.atam@gmail.com>
2023-05-15 14:27:35 +0100

    U

--- resources/views/emails/receipt/index.blade.php
+++ resources/views/emails/receipt/index.blade.php
@@ -846,11 +846,9 @@
                                                       <td class="wz2" height="10" width="30"></td>
                                                    </tr>
                                                    <tr>
-                                                      <td class="wz2" width="30"><br></td>
                                                       <td class="rt5td" data-link-style="text-decoration:none; color:#67bffd;" data-link-color="RegularLink" data-color="RegularTXT" style="color: #425065;font-family: sans-serif;font-size: 14px;font-weight: lighter;text-align: center;line-height: 23px;">
                                                          <div href="#" target="_blank" data-color="RegularLink" style="text-decoration: none;"> ₦{{ number_format($order->heavy_item_price) }} </div>
                                                       </td>
-                                                      <td class="wz2" width="30"><br></td>
                                                    </tr>
                                                    <tr>
                                                       <td class="wz2" height="10" width="30"></td>
@@ -867,11 +865,9 @@
                                                       <td class="wz2" height="10" width="30"></td>
                                                    </tr>
                                                    <tr>
-                                                      <td class="wz2" width="30"><br></td>
                                                       <td class="rt5td" data-link-style="text-decoration:none; color:#67bffd;" data-link-color="RegularLink" data-color="RegularTXT" style="color: #425065;font-family: sans-serif;font-size: 14px;font-weight: lighter;text-align: center;line-height: 23px;">
                                                          <div data-color="RegularLink" style="text-decoration: none;color: #67bffd;">Discount</div>
                                                       </td>
-                                                      <td class="wz2" width="30"><br></td>
                                                    </tr>
                                                    <tr>
                                                       <td class="wz2" height="10" width="30"></td>
@@ -884,11 +880,9 @@
                                                       <td class="wz2" height="10" width="30"></td>
                                                    </tr>
                                                    <tr>
-                                                      <td class="wz2" width="30"><br></td>
                                                       <td class="rt5td" data-link-style="text-decoration:none;" data-link-color="RegularLink" data-color="RegularTXT" style="color: #425065;font-family: sans-serif;font-size: 14px;font-weight: lighter;text-align: center;line-height: 23px;">
                                                          <div href="#" target="_blank" data-color="RegularLink" style="text-decoration: none;"> {{ $order->coupon_value }} </div>
                                                       </td>
-                                                      <td class="wz2" width="30"><br></td>
                                                    </tr>
                                                    <tr>
                                                       <td class="wz2" height="10" width="30"></td>
