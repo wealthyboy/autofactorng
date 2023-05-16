4f954039245b581859a7263ece32ce4447c10192
Jacob Atam <jacob.atam@gmail.com>
2023-05-16 13:32:21 +0100

    U

--- app/Http/Controllers/Products/ProductsController.php
+++ app/Http/Controllers/Products/ProductsController.php
@@ -46,8 +46,8 @@ class ProductsController extends Controller
             return (new ProductsCollection($products))
                 ->additional([
                     'string' => $this->buildSearchString($request),
-                    'showFitStringOnCategoryPage' => true,
-                    'showSearch' => true,
+                    'showFitStringOnCategoryPage' => $this->getCategory($category)  && null != $this->buildSearchString($request) ? true : false,
+                    'showSearch' => $this->showSearch($category),
                     'productFitString' => null,
                     'fits' =>  $this->buildSearchString($request) ? true : false,
                     'search_filters' => null
@@ -118,8 +118,8 @@ class ProductsController extends Controller
             return (new ProductsCollection($products))
                 ->additional([
                     'string' => $cat,
-                    'showSearch' => false,
-                    'showSearch' => $this->showSearch($category),
+                    'showFitStringOnCategoryPage' => true,
+                    'showSearch' => true,
                     'productFitString' => null,
                     'fits' =>  $this->buildSearchString($request) ? true : false,
                     'search_filters' => null
