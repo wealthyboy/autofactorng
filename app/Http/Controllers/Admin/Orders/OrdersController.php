05beddf1b39f32fc63a08ade29e8bb7604f3f9a2
Jacob Atam <jacob.atam@gmail.com>
2023-05-16 16:26:15 +0100

    U

--- app/Http/Controllers/Admin/Orders/OrdersController.php
+++ app/Http/Controllers/Admin/Orders/OrdersController.php
@@ -83,7 +83,7 @@ class OrdersController extends Table
 	public function store(Request $request)
 	{
 
-
+        dd(true);
 
 		try {
 			DB::beginTransaction();
@@ -175,14 +175,11 @@ class OrdersController extends Table
 			}
 
 
-
-
 			try {
 				$when = now()->addMinutes(5);
 				Mail::to($user->email)
 					->bcc('damilola@autofactorng.com')
 					->bcc('jacob.atam@gmail.com')
-
 					->send(new OrderReceipt($order, null, null, $sub_total));
 			} catch (\Throwable $th) {
 				Log::info("Mail error :" . $th);
