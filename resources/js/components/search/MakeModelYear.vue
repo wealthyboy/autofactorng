6c6a30a0a8c0feb7b2201aadcf2f869e7eacf922
Jacob Atam <jacob.atam@gmail.com>
2023-05-16 13:06:18 +0100

    U

--- resources/js/components/search/MakeModelYear.vue
+++ resources/js/components/search/MakeModelYear.vue
@@ -118,8 +118,7 @@ export default {
         const { getProducts } = useActions(["getProducts"]);
 
         onMounted(() => {
-            console,log(years.length)
-            if (!years.length) {
+            if (!years.value.length) {
                 axios
                 .get("/api/years")
                 .then((response) => {                  
