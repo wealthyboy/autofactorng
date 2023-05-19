b6d5a4294151dce5a057ac57122976afba50fdbe
Jacob Atam <jacob.atam@gmail.com>
2023-05-19 18:00:32 +0100

    UPDATED no t yet

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container-fluid px-0">
     <div class="row  align-items-start">
-        <div class="col-xl-5 col-lg-5 col-md-5 col-12">
+        <div class="col-xl-5 col-lg-5 col-md-12 col-12">
 
             @include('_partials.back')
 
