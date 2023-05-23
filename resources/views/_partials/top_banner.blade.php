2f59e5aa7cbb1fe60517de61add287b20acb6f93
Jacob Atam <jacob.atam@gmail.com>
2023-05-23 18:46:13 +0100

    email removed from orders

--- resources/views/_partials/top_banner.blade.php
+++ resources/views/_partials/top_banner.blade.php
@@ -1,6 +1,6 @@
 @if (null !== $global_promo)
 <div class="container-fluid text-center mt-3">
-    <div  class=" top-notice py-sm-4 py-md-2 text-white mb-2">
+    <div  class="">
         <div class="row">
            <img src="{{ $top_banner->image }}" class="{{ $top_banner->device }}" alt="" srcset="">
         </div>
