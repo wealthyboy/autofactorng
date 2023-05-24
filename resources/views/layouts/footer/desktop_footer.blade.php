775d668f501421fbbaad9bc0dec2d80b40707900
Jacob Atam <jacob.atam@gmail.com>
2023-05-24 22:54:34 +0100

    Updated css

--- resources/views/layouts/footer/desktop_footer.blade.php
+++ resources/views/layouts/footer/desktop_footer.blade.php
@@ -4,10 +4,10 @@
         <div class="widget">
             <h2 class="widget-title text-white mb-">{{ title_case($info->name) }}</h2>
             @if($info->children->count())
-            <ul class="links text-secondry list-unstyled">
+            <ul class="links text-secondry list-unstyled  text-white">
                 @foreach($info->children as $info)
-                <li class="py-3">
-                    <a href="{{ $info->c_link }}">
+                <li class="py-3  text-white">
+                    <a class=" text-white" href="{{ $info->c_link }}">
                         {{ $info->name }}
                     </a>
                 </li>
