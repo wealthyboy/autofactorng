7f415c98f3bcce667cb27b3065ef20ae263f9143
Jacob Atam <jacob.atam@gmail.com>
2023-05-24 22:50:55 +0100

    Updated css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -300,11 +300,11 @@
 
       <!-- End .main -->
 
-      <footer class="footer mt-auto py-3 bg-light">
+      <div class="footer mt-auto py-3 bg-light">
   <div class="container">
     <span class="text-muted">Place sticky footer content here.</span>
   </div>
-</footer>
+</div>
 
       
    </div>
