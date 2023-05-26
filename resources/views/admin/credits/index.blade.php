b0bae30fb1ca570adc339110369cafb54f6ff9a9
Jacob Atam <jacob.atam@gmail.com>
2023-05-26 17:52:39 +0100

    UPDATED BUTTON

--- resources/views/admin/credits/index.blade.php
+++ resources/views/admin/credits/index.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.layouts.app')
 @section('content')
 <div class="row">
-   @include('admin._partials.t', ['models' => $users, 'name' => 'Customers'])
+   @include('admin._partials.t', ['models' => $subcribers, 'name' => 'Customers'])
 </div>
 @endsection
 @section('inline-scripts')
