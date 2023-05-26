f4d685d75571554cc532fe75f67d5d4f11f3c605
Jacob Atam <jacob.atam@gmail.com>
2023-05-26 17:53:38 +0100

    UPDATED BUTTON

--- routes/web.php
+++ routes/web.php
@@ -30,8 +30,9 @@ Route::group(['middleware' => 'admin', 'prefix' => 'admin'], function () {
     Route::get('account/filter', 'Admin\Account\AccountsController@index')->name('filter_sales');
     Route::post('customers/wallet/{id}', 'Admin\Customers\CustomersController@fundWallet');
     Route::post('/update/status', 'Admin\Orders\OrdersController@updateStatus');
-    Route::resource('blogs',  'Admin\Blog\BlogController',['names' => 'blogs']);
     Route::resource('credits',  'Admin\AutoCredit\AutoCreditController',['names' => 'credits']);
+
+    Route::resource('blogs',  'Admin\Blog\BlogController',['names' => 'blogs']);
 
 
 
