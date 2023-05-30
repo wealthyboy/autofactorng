867ff9e9ded1e17dfa33e819e8dbff9b382df3d5
Jacob Atam <jacob.atam@gmail.com>
2023-05-30 19:46:21 +0100

    Fix

--- resources/views/admin/orders/invoice.blade.php
+++ resources/views/admin/orders/invoice.blade.php
@@ -120,7 +120,6 @@ td{
 
       </div>
 
-      {{$ordered_products   }}
       
       <div class="info"> 
       <h6>
@@ -158,7 +157,7 @@ td{
                      @foreach($ordered_products as $ordered_product)
 
 							<tr class="service">
-								<td class="tableitem"><p class="itemtext">Communication</p></td>
+								<td class="tableitem"><p class="itemtext">{{ $ordered_product->Product}}</p></td>
 								<td class="tableitem"><p class="itemtext">5</p></td>
 								<td class="tableitem"><p class="itemtext">$375.00</p></td>
 							</tr>
