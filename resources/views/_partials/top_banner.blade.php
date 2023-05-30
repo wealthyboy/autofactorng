5fba3bbf539a191e2432d3112b1bc76b1a487c74
Jacob Atam <jacob.atam@gmail.com>
2023-05-30 18:47:49 +0100

    Fix

--- resources/views/_partials/top_banner.blade.php
+++ resources/views/_partials/top_banner.blade.php
@@ -1,9 +1,12 @@
 @if (null !== $global_promo)
-<div class="container-fluid text-center mt-3">
+<div class="{{  $top_banner->image  }}">
+<div class="container-fluid text-center mt-3  ">
     <div class="row">
         <div class="col-12">
            <img src="{{ $top_banner->image }}" class="{{ $top_banner->device }} img-fluid" alt="" srcset="">
         </div>
     </div><!-- End .container -->
 </div>
+</div>
+
 @endif
