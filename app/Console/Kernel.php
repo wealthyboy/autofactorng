a702c95c33dbe1ea6514460454956ed5e5ebc522
Jacob Atam <jacob.atam@gmail.com>
2023-06-01 13:04:16 +0100

    U

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('send:autocreditreminder')->everySixHours();
+        $schedule->command('send:autocreditexpiry')->everySixHours();
 
         $schedule->command('send:autocreditreminder')->everySixHours();
 
