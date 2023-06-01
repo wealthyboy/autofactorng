fd2d18082ab497d3adf57029ed73b53ff7298208
Jacob Atam <jacob.atam@gmail.com>
2023-06-01 10:01:12 +0100

    Fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('send:autocreditexpiryreminder')->dailyAt('18:40');
+        $schedule->command('send:autocreditexpiryreminder')->dailyAt('10:40');
     }
 
     /**
