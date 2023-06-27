d74ff0696c44e0342108909ddff91c3aa4578524
Jacob <jacob.atam@gmail.com>
2023-06-27 13:08:09 -0700

    Updated Cart

--- app/Http/Controllers/Products/ProductsController.php
+++ app/Http/Controllers/Products/ProductsController.php
@@ -279,7 +279,7 @@ class ProductsController extends Controller
             session(['model' => Attribute::find($request->model_id)->name]);
             session(['engine' => optional(Attribute::find($request->engine_id))->name]);
             session(['year' => $request->year]);
-            dd(Attribute::find($request->engine_id)->name);
+            dd(Attribute::find($request->engine_id));
         }
 
         if ($request->checkForCategory == true && $this->getCategory($category)) {
