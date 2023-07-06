a7678fa6b7f3e7f83cc5759b605d018057733405
Jacob <jacob.atam@gmail.com>
2023-07-06 08:06:50 -0700

    fIX

--- resources/views/admin/orders/invoice.blade.php
+++ resources/views/admin/orders/invoice.blade.php
@@ -229,7 +229,7 @@
       <div id="client">
         <div class="to">INVOICE TO:</div>
         <h2 class="name">{{ $order->first_name  .' '.  $order->last_name }}</h2>
-        <div class="address">{{ $order->address }},<br /> {{ $order->city }} {{ $order->state }}</div>
+        <div class="address">{{ $order->address }}, {{ $order->city }} {{ $order->state }}</div>
         <div class="email"><a href="mailto:john@example.com">{{ $order->email }}</a></div>
         <div class="email"><a href="tel:{{ $order->phone }}">{{ $order->phone_number }}</a></div>
 
