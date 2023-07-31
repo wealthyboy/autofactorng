d499bc17fc777e45339a94a8ca23e7d8a81246f0
Jacob <jacob.atam@gmail.com>
2023-07-31 12:05:08 -0700

    Updated Products

--- app/Http/Controllers/Api/Address/AddressController.php
+++ app/Http/Controllers/Api/Address/AddressController.php
@@ -60,8 +60,9 @@ class AddressController extends Controller
         $prices['isLagos'] = $is_lagos;
 
         foreach ($carts as $key => $cart) {
-            dd($cart->product);
             if ($cart->product->condition_is_present) {
+                dd(true);
+
                 $heavy_item_prices = ShippingRate::where(['product_id' => $cart->product_id, 'is_lagos' => $is_lagos])->get();
                 foreach ($heavy_item_prices as $heavy_item_price) {
                     if ($heavy_item_price->condition == '=') {
