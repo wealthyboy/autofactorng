dd3e0c566d69c99cdf66a472503fd7711c167e94
Jacob <jacob.atam@gmail.com>
2023-08-14 12:05:56 -0700

    Fix

--- resources/views/admin/errors/message.blade.php
+++ resources/views/admin/errors/message.blade.php
@@ -1,8 +1,8 @@
-@if(session($message))
+@if(session('message'))
 <div class="alert alert-primary alert-dismissible text-white" role="alert">
     @foreach ($errors->all() as $error )
     <li class="list-unstyled" style="padding-left: 5px;"> &nbsp;&nbsp;<i class="fa fa-exclamation-circle"></i>
-        <span class="text-sm text-white"> {{ session($message) }} </span>
+        <span class="text-sm text-white"> {{ session('message') }} </span>
     </li>
     @endforeach
     <button type="button" class="btn-close text-lg py-3 opacity-10" data-bs-dismiss="alert" aria-label="Close">
