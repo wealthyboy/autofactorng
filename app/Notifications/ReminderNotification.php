312af3e7205d13454cbc673af515e8889ae97a3b
Jacob <jacob.atam@gmail.com>
2023-08-15 22:58:47 -0700

    Fix

--- app/Notifications/ReminderNotification.php
+++ app/Notifications/ReminderNotification.php
@@ -56,7 +56,6 @@ class ReminderNotification extends Notification
     {
         $m = (new MailMessage)
             ->bcc('account@autofactorng.com')
-            ->bcc('jacob.atam@gmail.com')
             ->bcc('care@autofactorng.com')
             ->subject($this->subject)
             ->greeting('Hello ' . $this->user->name);
