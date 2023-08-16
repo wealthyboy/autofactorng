ca708a478337e7abf7c289c8fe69d3972961262a
Jacob <jacob.atam@gmail.com>
2023-08-15 22:53:00 -0700

    Fix

--- resources/views/admin/orders/invoice.blade.php
+++ resources/views/admin/orders/invoice.blade.php
@@ -244,7 +244,7 @@
         <h1>INVOICE </h1>
         <div class="date">Date of Invoice: {{ $order->created_at->format('d/m/y') }}</div>
         <div class="date">Invoice No: {{ $order->invoice }}</div>
-        <div class="date">Payment Type: {{ ucfirst($order->payment_type) ?? 'Payment on delivery'}}</div>
+        <div class="date">Payment Type: {{ ucfirst(implode(' ',explode('_',$order->payment_type))) ?? 'Payment on delivery'}}</div>
 
 
       </div>
