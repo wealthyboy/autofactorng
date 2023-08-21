015783d0d9c0b25e94479b11ccc3266a22e8d72a
Jacob <jacob.atam@gmail.com>
2023-08-21 06:21:56 -0700

    debug

--- app/Http/Controllers/Admin/Image/ImagesController.php
+++ app/Http/Controllers/Admin/Image/ImagesController.php
@@ -114,15 +114,15 @@ class ImagesController extends Controller
                 );
 
                 $canvas = \Image::canvas(600, 600);
-                try {
 
-                    $image  = \Image::make($path)->resize(600, 600, function ($constraint) {
-                        $constraint->aspectRatio();
-                    });
-                    $canvas->insert($image, 'center');
-                    $canvas->save(
-                        public_path('images/products/l/' . $file)
-                    );
+
+                $image  = \Image::make($path)->resize(600, 600, function ($constraint) {
+                    $constraint->aspectRatio();
+                });
+                $canvas->insert($image, 'center');
+                $canvas->save(
+                    public_path('images/products/l/' . $file)
+                );
 
                 $canvas = \Image::canvas(150, 200);
                 $image  = \Image::make($path)->resize(150, 200, function ($constraint) {
