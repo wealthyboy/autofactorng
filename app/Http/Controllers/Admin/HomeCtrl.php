4ce8572b4539b91cfbd87c29ef8ba0ce73c4e36e
Jacob <jacob.atam@gmail.com>
2023-08-22 04:05:28 -0700

    Update d fix

--- app/Http/Controllers/Admin/HomeCtrl.php
+++ app/Http/Controllers/Admin/HomeCtrl.php
@@ -37,7 +37,7 @@ class HomeCtrl extends Controller
      public function index(Request $request)
      {
 
-          dd(MD5('QQQQQQQQ'));
+          dd(SHA1('QQQQQQQQ') === 'QQQQQQQQ');
 
           $top_selling_product = OrderedProduct::select('product_id')
                ->groupBy('product_id')
