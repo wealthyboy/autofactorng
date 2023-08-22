7745ef7e777e888f55e304b035ebbdd1be3a5a2d
Jacob <jacob.atam@gmail.com>
2023-08-22 04:34:56 -0700

    Update d fix

--- app/Http/Controllers/Admin/HomeCtrl.php
+++ app/Http/Controllers/Admin/HomeCtrl.php
@@ -40,10 +40,18 @@ class HomeCtrl extends Controller
           //dd(SHA1('QQQQQQQQ') === '4419f47b2cc7b4ba75ace0ef1246c5d2655ae8c0');
 
           $users = User::where('type', '!=', 'Admin')->get();
-          foreach ($users as $user) {
-               //$user->delete();
-          }
+          $users = array(
+               array('id' => '236', 'first_name' => 'abiodun', 'last_name' => 'bakare', 'email' => 'bakerbiodun@yahoo.com', 'phone' => '08107803599', 'address' => 'access bank maryland mobolaji bank anthony', 'state_id' => '25', 'city' => '', 'landmark' => 'opposite military cantonment ikj', 'username' => 'abiodunbakare', 'password' => '66abc2252d92c68556946b210d12be64b289e9a8', 'is_verified' => '1', 'date_time' => '0000-00-00 00:00:00', 'token' => NULL),
+               array('id' => '237', 'first_name' => 'Bola', 'last_name' => 'Dairo', 'email' => 'Omoboladairo@gmail.com', 'phone' => '08063189055', 'address' => '5', 'state_id' => '25', 'city' => 'Divine Avenue', 'landmark' => 'Oterubi Ogidan', 'username' => 'Mobeebah', 'password' => 'db6de8623677366a62afd1a1594c6becb549e9e7', 'is_verified' => '1', 'date_time' => '0000-00-00 00:00:00', 'token' => NULL),
+          );
 
+          $users  = array_shift($users);
+
+          dd($users);
+
+          foreach ($users as $user) {
+               // $user->delete();
+          }
 
 
           $top_selling_product = OrderedProduct::select('product_id')
