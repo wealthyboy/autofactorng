ee64a40da738627a0cffb41f757489636efcc067
Jacob <jacob.atam@gmail.com>
2023-08-22 04:45:53 -0700

    Update d fix

--- app/Http/Controllers/Admin/HomeCtrl.php
+++ app/Http/Controllers/Admin/HomeCtrl.php
@@ -39,33 +39,31 @@ class HomeCtrl extends Controller
 
           //dd(SHA1('QQQQQQQQ') === '4419f47b2cc7b4ba75ace0ef1246c5d2655ae8c0');
 
-          $users = User::where('type', '!=', 'Admin')->get();
-          // $users = array(
-          //      array('id' => '236', 'first_name' => 'abiodun', 'last_name' => 'bakare', 'email' => 'bakerbiodun@yahoo.com', 'phone' => '08107803599', 'address' => 'access bank maryland mobolaji bank anthony', 'state_id' => '25', 'city' => '', 'landmark' => 'opposite military cantonment ikj', 'username' => 'abiodunbakare', 'password' => '66abc2252d92c68556946b210d12be64b289e9a8', 'is_verified' => '1', 'date_time' => '0000-00-00 00:00:00', 'token' => NULL),
-          //      array('id' => '237', 'first_name' => 'Bola', 'last_name' => 'Dairo', 'email' => 'Omoboladairo@gmail.com', 'phone' => '08063189055', 'address' => '5', 'state_id' => '25', 'city' => 'Divine Avenue', 'landmark' => 'Oterubi Ogidan', 'username' => 'Mobeebah', 'password' => 'db6de8623677366a62afd1a1594c6becb549e9e7', 'is_verified' => '1', 'date_time' => '0000-00-00 00:00:00', 'token' => NULL),
-          // );
+          $users = array(
+               array('id' => '236', 'first_name' => 'abiodun', 'last_name' => 'bakare', 'email' => 'bakerbiodun@yahoo.com', 'phone' => '08107803599', 'address' => 'access bank maryland mobolaji bank anthony', 'state_id' => '25', 'city' => '', 'landmark' => 'opposite military cantonment ikj', 'username' => 'abiodunbakare', 'password' => '66abc2252d92c68556946b210d12be64b289e9a8', 'is_verified' => '1', 'date_time' => '0000-00-00 00:00:00', 'token' => NULL),
+               array('id' => '237', 'first_name' => 'Bola', 'last_name' => 'Dairo', 'email' => 'Omoboladairo@gmail.com', 'phone' => '08063189055', 'address' => '5', 'state_id' => '25', 'city' => 'Divine Avenue', 'landmark' => 'Oterubi Ogidan', 'username' => 'Mobeebah', 'password' => 'db6de8623677366a62afd1a1594c6becb549e9e7', 'is_verified' => '1', 'date_time' => '0000-00-00 00:00:00', 'token' => NULL),
+          );
 
 
 
           foreach ($users as $user) {
 
-               $user->delete();
-               // $u = new User;
-               // $u->id = $user['id'];
-               // $u->name = $user['first_name'];
-               // $u->last_name = $user['last_name'];
-               // $u->email = $user['email'];
-               // $u->phone_number =  $user['phone'];
-               // // $user->address = $user[];
-               // // $user->state_id = $user[];
-               // // $user->landmark =  $user[];
-               // $u->password =  bcrypt($user['password']);
-               // $u->old_password = $user['password'];
-               // $u->is_verified =  1;
-               // $u->created_at =  now();
-               // $u->is_old = 1;
-               // $u->type = 'subscriber';
-               // $u->save();
+               $u = new User;
+               $u->id = $user['id'];
+               $u->name = $user['first_name'];
+               $u->last_name = $user['last_name'];
+               $u->email = $user['email'];
+               $u->phone_number =  $user['phone'];
+               // $user->address = $user[];
+               // $user->state_id = $user[];
+               // $user->landmark =  $user[];
+               $u->password =  bcrypt($user['password']);
+               $u->old_password = $user['password'];
+               $u->is_verified =  1;
+               $u->created_at =  now();
+               $u->is_old = 1;
+               $u->type = 'subscriber';
+               $u->save();
           }
 
 
