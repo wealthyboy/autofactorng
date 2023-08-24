ed16072779bd7bb826daaed3322a85d5433c55a0
Jacob <jacob.atam@gmail.com>
2023-08-23 21:11:51 -0700

    Fim order email

---
+++ database/migrations/2023_08_24_033710_create_order_emails_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderEmailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_emails', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_id')->nullable();
+            $table->string('fullname')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('payment')->nullable();
+            $table->string('landmark')->nullable();
+            $table->string('address')->nullable();
+            $table->string('email')->nullable();
+            $table->string('shipping')->nullable();
+            $table->integer('state_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_emails');
+    }
+}
