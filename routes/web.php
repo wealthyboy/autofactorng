8e715d81f2fe3f30ac58ec8a2c5f0d050bb7ee43
Jacob <jacob.atam@gmail.com>
2023-08-25 09:07:56 -0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
 Route::group(['middleware' => 'admin', 'prefix' => 'admin'], function () {
 
     Route::get('/', 'Admin\HomeController@index')->name('admin_home');
