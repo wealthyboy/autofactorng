ab0a69308bcb347c946a04024d560b986f141c21
jacobatam <jacob.atam@gmail.com>
2023-08-30 00:43:24 -0700

    Update Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -133,7 +133,7 @@ class Order extends Model
 		try {
 			$when = now()->addMinutes(5);
 			Mail::to($user->email)
-				->bcc('care@autofactorng.com')
+				->cc('care@autofactorng.com')
                 ->bcc('info@autofactorng.com')
 
 				->send(new OrderReceipt($order, null, null, $sub_total));
