0073326a49691d57bd2f7304fac314df1ad58293
Jacob <jacob.atam@gmail.com>
2023-09-02 02:45:53 -0700

    Fix mailchimp

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -105,7 +105,7 @@ class RegisterController extends Controller
             $Mailchimp_Lists->subscribe(
                 config('services.mailchimp.list'),
                 $data['email'],
-                null,   // Set the first name and last name for the new subscriber.
+                array('FNAME' => 'jacob', 'EMAIL' => $data['email'], 'LNAME' => 'asaAS'),   // Set the first name and last name for the new subscriber.
                 'text',    // Specify the e-mail message type: 'html' or 'text'
                 FALSE,     // Set double opt-in: If this is set to TRUE, the user receives a message to confirm they want to be added to the list.
                 TRUE
