2aeeee1245a3febec6303d519a4a4538f1f80a59
Jacob <jacob.atam@gmail.com>
2023-09-02 04:29:30 -0700

    Fix mailchimp

--- app/Http/Controllers/Api/NewsLetter/NewsLetterController.php
+++ app/Http/Controllers/Api/NewsLetter/NewsLetterController.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Model\Newsletter;
 use App\Services\Newsletter\Contracts\NewsletterContract;
 use App\Services\Newsletter\Exceptions\UserAlreadySubscribedException;
-
+use Illuminate\Support\Facades\Log;
 
 class NewsLetterController extends Controller
 {
@@ -55,21 +55,40 @@ class NewsLetterController extends Controller
         //new Review Notification
         //Sign up to mail chimp
 
+        $email = $request->email;
+        $list_id = config('services.mailchimp.list');
+        $api_key = config('services.mailchimp.secret');
+        $data_center = substr($api_key, strpos($api_key, '-') + 1);
+
+        $url = 'https://' . $data_center . '.api.mailchimp.com/3.0/lists/' . $list_id . '/members';
+
+        $json = json_encode([
+            'email_address' => $email,
+            'status'        => 'subscribed', //pass 'subscribed' or 'pending'
+        ]);
+
         try {
-            $mergeVar = ['FNAME' => '', 'LNAME' => ''];
-            $this->newsletter->subscribe(
-                config('services.mailchimp.list'),
-                $request->email,
-                $mergeVar
-            );
-        } catch (UserAlreadySubscribedException $e) {
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_USERPWD, 'user:' . $api_key);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+            $result = curl_exec($ch);
+            $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            curl_close($ch);
+        } catch (\Exception $e) {
             return response()->json(
                 [
                     'message' => $e->getMessage()
                 ],
-                200
+                400
             );
         }
+
+
 
 
         return response()->json(
