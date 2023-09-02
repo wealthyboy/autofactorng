82a5dbe828993c43b4948149102118093de93d90
Jacob <jacob.atam@gmail.com>
2023-09-02 03:28:18 -0700

    Fix mailchimp

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -104,10 +104,44 @@ class RegisterController extends Controller
 
             $Mailchimp_Lists->subscribe(
                 config('services.mailchimp.list'),
-                array('email' => $data['email']),
+                'email_address' => $data['email']),
                 array('FNAME' => 'jacob', 'LNAME' => 'asaAS'),   // Set the first name and last name for the new subscriber.
 
             );
+
+
+            $email = $data['email'];
+            $list_id = config('services.mailchimp.list');
+            $api_key = config('services.mailchimp.secret');
+
+            $data_center = substr($api_key,strpos($api_key,'-')+1);
+
+            $url = 'https://'. $data_center .'.api.mailchimp.com/3.0/lists/'. $list_id .'/members';
+
+            $json = json_encode([
+                'email_address' => $email,
+                'status' => 'subscribed', //pass 'subscribed' or 'pending'
+            ]);
+
+            try {
+                $ch = curl_init($url);
+                curl_setopt($ch, CURLOPT_USERPWD, 'user:' . $api_key);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+                curl_setopt($ch, CURLOPT_POST, 1);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+                $result = curl_exec($ch);
+                $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+                curl_close($ch);
+
+                if (200 == $status_code) {
+                    echo "The user added successfully to the Mailchimp.";
+                }
+            } catch(Exception $e) {
+                echo $e->getMessage();
+            }
         } catch (UserAlreadySubscribedException $e) {
             dd($e->getMessage());
         }
