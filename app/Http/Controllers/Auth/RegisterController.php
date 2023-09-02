51e48bd6b507207f5d07adb323c68326bb5eaf5b
Jacob <jacob.atam@gmail.com>
2023-09-02 03:34:40 -0700

    Fix mailchimp

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Notification;
 use App\Services\Newsletter\Contracts\NewsletterContract;
 use App\Services\Newsletter\Exceptions\UserAlreadySubscribedException;
 use App\Services\Newsletter\MailChimpNewsletter;
+use Illuminate\Support\Facades\Log;
 use Mailchimp;
 use Mailchimp_Lists;
 
@@ -97,53 +98,39 @@ class RegisterController extends Controller
             'phone_number' => $data['phone_number']
         ]);
 
+
+
+        $email = $data['email'];
+        $list_id = config('services.mailchimp.list');
+        $api_key = config('services.mailchimp.secret');
+
+        $data_center = substr($api_key, strpos($api_key, '-') + 1);
+
+        $url = 'https://' . $data_center . '.api.mailchimp.com/3.0/lists/' . $list_id . '/members';
+
+        $json = json_encode([
+            'email_address' => $email,
+            'status'        => 'subscribed', //pass 'subscribed' or 'pending'
+            'merge_fields'  => [
+                'FNAME' =>  $data['name'],
+                'LNAME' =>  $data['last_name']
+            ]
+        ]);
+
         try {
-
-            // $Mailchimp = new Mailchimp(config('services.mailchimp.secret'));
-            // $Mailchimp_Lists = new Mailchimp_Lists($Mailchimp);
-
-            // $Mailchimp_Lists->subscribe(
-            //     config('services.mailchimp.list'),
-            //     'email_address' => $data['email']),
-            //     array('FNAME' => 'jacob', 'LNAME' => 'asaAS'),   // Set the first name and last name for the new subscriber.
-
-            // );
-
-
-            $email = $data['email'];
-            $list_id = config('services.mailchimp.list');
-            $api_key = config('services.mailchimp.secret');
-
-            $data_center = substr($api_key, strpos($api_key, '-') + 1);
-
-            $url = 'https://' . $data_center . '.api.mailchimp.com/3.0/lists/' . $list_id . '/members';
-
-            $json = json_encode([
-                'email_address' => $email,
-                'status' => 'subscribed', //pass 'subscribed' or 'pending'
-            ]);
-
-            try {
-                $ch = curl_init($url);
-                curl_setopt($ch, CURLOPT_USERPWD, 'user:' . $api_key);
-                curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-                curl_setopt($ch, CURLOPT_POST, 1);
-                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-                curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
-                $result = curl_exec($ch);
-                $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-                curl_close($ch);
-
-                if (200 == $status_code) {
-                    echo "The user added successfully to the Mailchimp.";
-                }
-            } catch (Exception $e) {
-                echo $e->getMessage();
-            }
-        } catch (UserAlreadySubscribedException $e) {
-            dd($e->getMessage());
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_USERPWD, 'user:' . $api_key);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+            $result = curl_exec($ch);
+            $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            curl_close($ch);
+        } catch (\Exception $e) {
+            Log::info($e);
         }
 
 
