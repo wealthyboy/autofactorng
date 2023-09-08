149daf502d38e6149f5626d49406dd7bb33660ba
Jacob <jacob.atam@gmail.com>
2023-09-07 19:07:58 -0700

    Fix download

---
+++ app/Exports/ActivityExport.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\Customer;
+use App\Models\CustomerWarehouse;
+use App\Models\User;
+use Carbon\Carbon;
+
+class CustomerExport extends Exporter
+{
+  public function headings(): array
+  {
+    return [
+      'Customer ID',
+      'Customer Since',
+      'First Name',
+      'Last Name',
+      'Phone',
+      'Email',
+      'Lead',
+    ];
+  }
+
+  public function collection()
+  {
+    ini_set('memory_limit', -1);
+    // $items = data_get($this->filter, 'items', []);
+    $query = User::query();
+    // if (!data_get($this->filter, 'all_items')) {
+    //   $query->whereIn('customer_id', $items);
+    // }
+
+    $users = $query->get();
+
+    return $users->map(function (User $user) {
+
+      return [
+        $user->id,
+        Carbon::parse($user->created_at)->format('m-d-Y'),
+        $user->first_name,
+        $user->last_name,
+        $user->phone_number,
+        $user->email,
+      ];
+    });
+  }
+}
---
+++ app/Exports/ProductsExport.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\Customer;
+use App\Models\CustomerWarehouse;
+use App\Models\Product;
+use App\Models\User;
+use Carbon\Carbon;
+
+class CustomerExport extends Exporter
+{
+  public function headings(): array
+  {
+    return [
+      "Id",
+      "Image",
+      "Name",
+      "Category",
+      "Price",
+      "Date Added",
+    ];
+  }
+
+  public function collection()
+  {
+    ini_set('memory_limit', -1);
+    // $items = data_get($this->filter, 'items', []);
+    $query = Product::query();
+    // if (!data_get($this->filter, 'all_items')) {
+    //   $query->whereIn('customer_id', $items);
+    // }
+
+    $products = $query->get();
+
+    return $products->map(function (Product $product) {
+      $price =  $product->discounted_price ?  $product->discounted_price . ' - ' . $product->price : $product->price;
+
+      return [
+        $product->id,
+        $product->image_m,
+        $product->name,
+        implode(', ', $product->categories->pluck('name')->toArray()),
+        $price,
+        $product->created_at->format('d-m-y'),
+      ];
+    });
+  }
+}
