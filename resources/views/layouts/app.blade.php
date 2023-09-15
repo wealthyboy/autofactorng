efa0d3e77a574b580db5583ed5d46324f0c6df10
Jacob <jacob.atam@gmail.com>
2023-09-15 00:29:29 -0700

    Fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,44 +9,44 @@
 
    <title>{{ isset( $page_title) ?  $page_title .' |  '.config('app.name') :  $system_settings->meta_title  }}</title>
 
-   <<meta name="description" content="{{ isset($page_meta_description) ? $page_meta_description : $system_settings->meta_description }}">
-      <meta name="keywords" content="{{ isset($meta_tag_keywords) ? $meta_tag_keywords : $system_settings->meta_tag_keywords }}" />
-      <link rel="canonical" href="{{ Config('app.url') }}">
-      <meta name="author" content="AuofactorNG">
+   <meta name="description" content="{{ isset($page_meta_description) ? $page_meta_description : $system_settings->meta_description }}">
+   <meta name="keywords" content="{{ isset($meta_tag_keywords) ? $meta_tag_keywords : $system_settings->meta_tag_keywords }}" />
+   <link rel="canonical" href="{{ Config('app.url') }}">
+   <meta name="author" content="AuofactorNG">
 
-      <link rel="stylesheet" preload href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+   <link rel="stylesheet" preload href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
-      <!-- Favicon -->
-      <!-- <link rel="icon" type="image/x-icon" href="assets/images/icons/favicon.png"> -->
-      <!-- Main CSS File -->
+   <!-- Favicon -->
+   <!-- <link rel="icon" type="image/x-icon" href="assets/images/icons/favicon.png"> -->
+   <!-- Main CSS File -->
 
-      <link rel="preload" href="/css/app.css?id={{ rand(1,2000)}}" as="style">
-      <script rel="preload" src="/js/jquery.min.js" as="script"></script>
-      <script rel="preload" src="/js/scripts.js?id={{ rand(1,2000)}}" as="script"></script>
+   <link rel="stylesheet" href="/css/app.css?id={{ rand(1,2000)}}">
+   <script src="/js/jquery.min.js"></script>
+   <script src="/js/scripts.js?id={{ rand(1,2000)}}"></script>
 
 
-      <meta property="og:site_name" content="Autofactorng Co">
-      <meta property="og:url" content="https://autofactorng.com/">
-      <meta property="og:title" content=" autofactorng">
-      <meta property="og:type" content="website">
-      <meta property="og:description" content="{{ isset($page_meta_description) ? $page_meta_description : $system_settings->meta_description }}">
-      <meta property="og:image:alt" content="">
-      <meta name="twitter:site" content="@autofactorng">
-      <meta name="twitter:card" content="summary_large_image">
-      <meta name="twitter:title" content="{{ isset($page_meta_description) ? $page_meta_description : $system_settings->meta_description }}">
-      <meta name="twitter:description" content="{{ isset($page_meta_description) ? $page_meta_description : $system_settings->meta_description }}">
+   <meta property="og:site_name" content="Autofactorng Co">
+   <meta property="og:url" content="https://autofactorng.com/">
+   <meta property="og:title" content=" autofactorng">
+   <meta property="og:type" content="website">
+   <meta property="og:description" content="{{ isset($page_meta_description) ? $page_meta_description : $system_settings->meta_description }}">
+   <meta property="og:image:alt" content="">
+   <meta name="twitter:site" content="@autofactorng">
+   <meta name="twitter:card" content="summary_large_image">
+   <meta name="twitter:title" content="{{ isset($page_meta_description) ? $page_meta_description : $system_settings->meta_description }}">
+   <meta name="twitter:description" content="{{ isset($page_meta_description) ? $page_meta_description : $system_settings->meta_description }}">
 
-      <!-- Google tag (gtag.js) -->
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-BZEG8EJ1VC"></script>
-      <script>
-         window.dataLayer = window.dataLayer || [];
+   <!-- Google tag (gtag.js) -->
+   <script async src="https://www.googletagmanager.com/gtag/js?id=G-BZEG8EJ1VC"></script>
+   <script>
+      window.dataLayer = window.dataLayer || [];
 
-         function gtag() {
-            dataLayer.push(arguments);
-         }
-         gtag('js', new Date());
-         gtag('config', 'G-BZEG8EJ1VC');
-      </script>
+      function gtag() {
+         dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+      gtag('config', 'G-BZEG8EJ1VC');
+   </script>
 
 
 </head>
