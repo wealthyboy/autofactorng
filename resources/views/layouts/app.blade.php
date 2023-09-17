09cf67706258a89ea03c55a698c144ab8d6b7213
Jacob <jacob.atam@gmail.com>
2023-09-17 09:37:07 -0700

    fix space

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,6 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="csrf-token" content="{{ csrf_token() }}">
    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-   {{dd( $page_title)}}
 
    <title>{{ isset($page_title) ? $page_title .' |  '.config('app.name') :  $system_settings->meta_title  }}</title>
 
