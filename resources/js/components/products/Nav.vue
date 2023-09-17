c35c060c224137079d70f27b74bb7b667e5ac08f
Jacob <jacob.atam@gmail.com>
2023-09-16 21:37:12 -0700

    fix space

--- resources/js/components/products/Nav.vue
+++ resources/js/components/products/Nav.vue
@@ -37,7 +37,7 @@
                 <div class="select-custom">
                     <select @change="sort($event)" name="orderby" class="form-control orderby">
                         <option value="" selected="selected">
-                            Sort
+                            Default
                         </option>
                         <option value="price,asc">Price: low to high</option>
                         <option value="price,desc">Price: high to low</option>
