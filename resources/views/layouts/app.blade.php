eed14bf163dbd59e3945a800e914b18d42621142
Jacob <jacob.atam@gmail.com>
2023-09-16 22:52:32 -0700

    fix space

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -175,7 +175,7 @@
                         </span>
                      </a>
                   </div>
-                  <div class="position-relative ">
+                  <div class="position-relative   me-md-4">
 
                      <a href="/login" class="d-flex flex-column align-items-center user" title="account">
                         <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
