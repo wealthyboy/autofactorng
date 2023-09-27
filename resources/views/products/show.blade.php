ef17304c4dda04d5a8005da74054fcd48abc6d28
Jacob <jacob.atam@gmail.com>
2023-09-26 17:25:23 -0700

    Fix

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -10,21 +10,21 @@
         </ol>
     </nav>
 
-    <div class="product-single-container products-show product-single-default">
-        <div class="row">
-            <div class="col-lg-1 d-none d-lg-block d-xl-block">
-                <div class="prod-thumbnail owl-dots flex-column">
-                    @foreach($product->images as $image)
-                    <div class="owl-dot mb-2">
-                        <img src="{{ $image->image_m }}" width="110" height="110" alt="product-thumbnail" />
-                    </div>
-                    @endforeach
+    <div class="product-single-container product-single-default product-center-vertical">
+        <div class="cart-message d-none">
+            <strong class="single-cart-notice">“Men Black Sports Shoes”</strong>
+            <span>has been added to your cart.</span>
+        </div>
 
-                </div>
-            </div>
-            <div class="col-lg-6  col-12 product-single-gallery">
-                <div class="product-slider-container">
+        <div class="row">
+
+
+            <div class="col-lg-6 product-single-gallery d-lg-flex order-0 order-lg-0">
+                <div class="product-slider-container mb-auto">
+
+
                     <div class="product-single-carousel owl-carousel owl-theme show-nav-hover">
+
                         @foreach($product->images as $image)
                         <div class="product-item">
                             <img class="product-single-image" src="{{ $image->image_l }}" width="468" height="468" alt="product" />
@@ -37,25 +37,22 @@
                     </span>
                 </div>
 
-                <div class="prod-thumbnail owl-dots d-block d-sm-none">
+                <div class="prod-thumbnail thumb-vertical owl-dots d-lg-block order-lg-first d-flex" id='carousel-custom-dots'>
+
+
                     @foreach($product->images as $image)
-                    <div class="owl-dot mb-2">
+                    <div class="owl-dot ">
                         <img src="{{ $image->image_m }}" width="110" height="110" alt="product-thumbnail" />
                     </div>
                     @endforeach
-
                 </div>
-
-
             </div>
-            <!-- End .product-single-gallery -->
 
             <show :product="{{ $product }}" />
 
-            <!-- End .product-single-details -->
         </div>
-        <!-- End .row -->
     </div>
+    <!-- End .product-single-container -->
 
     <div class="container-fluid  d-none d-lg-block">
         <div class="row">
