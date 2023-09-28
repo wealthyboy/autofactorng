6989408b9159e3e917e8df0770633c5f9a039064
Jacob <jacob.atam@gmail.com>
2023-09-28 10:28:19 -0700

    fix

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -235,7 +235,7 @@ class Product extends Model
             $price =  $product->discounted_price ?  $product->discounted_price . ' - ' . $product->price : $product->price;
             return [
                 "Id" => $product->id,
-                "Image" => $product->image_m,
+                "Image" => $product->image_tm,
                 "Name" => $product->name,
                 "Category" => implode(', ', $product->categories->pluck('name')->toArray()),
                 "Price" => $price,
