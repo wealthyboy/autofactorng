e7435958c2a223be0ed404420944dd9727a5f928
Jacob <jacob.atam@gmail.com>
2023-09-28 11:32:25 -0700

    Fix size

--- resources/views/_partials/categories.blade.php
+++ resources/views/_partials/categories.blade.php
@@ -6,7 +6,7 @@
 <div class="row g-0">
     @foreach($featured_categories as $key => $category)
     <div data-animation-name="fadeInUpShorter" class="col-6 col-lg-3 col-md-6 appear-animate">
-        <a href="{{ $category->link() }}" class="d-block p-0 border  py-5 no-hover">
+        <a href="{{ $category->link() }}" class="d-block p-0 border category-content  py-5 no-hover">
             <div class="d-flex justify-content-center align-items-center">
                 <div class="align-self-center text-center">
                     <div class="image-category">
