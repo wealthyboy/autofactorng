0bf785522c0269b70bef17b9ee39e988fd2d043b
Jacob <jacob.atam@gmail.com>
2023-10-01 14:47:40 -0700

    Fix

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -142,7 +142,7 @@
                         <td>
                            <div class="d-flex px-3 py-1">
                               <div>
-                                 <img src="{{ $statistics['top_product']->product->image_m }} " class="avatar me-3" alt="image">
+                                 <img src="{{ $statistics['top_product'] }} " class="avatar me-3" alt="image">
                               </div>
                               <div class="d-flex flex-column justify-content-center">
                                  <h6 class="mb-0 text-sm">{{ $statistics['top_product']->product_name }} </h6>
