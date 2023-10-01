6e9ecb2270d85c7c7be3de26321a88f1da620513
Jacob <jacob.atam@gmail.com>
2023-10-01 15:43:03 -0700

    Fix

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -67,56 +67,40 @@
             <div class="table-responsive">
                <table class="table align-items-center mb-0">
                   <thead>
-                     <tr>
-                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Companies</th>
-                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Members</th>
-                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Budget</th>
-                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Completion</th>
-                     </tr>
+
                   </thead>
                   <tbody>
                      <tr>
+                        @foreach($statistics['orders'] as $order)
                         <td>
                            <div class="d-flex px-2 py-1">
                               <div>
-                                 <img src="../assets/img/small-logos/logo-xd.svg" class="avatar avatar-sm me-3" alt="xd">
                               </div>
                               <div class="d-flex flex-column justify-content-center">
-                                 <h6 class="mb-0 text-sm">Material XD Version</h6>
+                                 <h6 class="mb-0 text-sm">{{ optional($order->orderEmail)->fullname  }}</h6>
                               </div>
                            </div>
                         </td>
                         <td>
-                           <div class="avatar-group mt-2">
-                              <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" aria-label="Ryan Tompson" data-bs-original-title="Ryan Tompson">
-                                 <img src="../assets/img/team-1.jpg" alt="team1">
-                              </a>
-                              <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" aria-label="Romina Hadid" data-bs-original-title="Romina Hadid">
-                                 <img src="../assets/img/team-2.jpg" alt="team2">
-                              </a>
-                              <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" aria-label="Alexander Smith" data-bs-original-title="Alexander Smith">
-                                 <img src="../assets/img/team-3.jpg" alt="team3">
-                              </a>
-                              <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" aria-label="Jessica Doe" data-bs-original-title="Jessica Doe">
-                                 <img src="../assets/img/team-4.jpg" alt="team4">
-                              </a>
-                           </div>
-                        </td>
-                        <td class="align-middle text-center text-sm">
-                           <span class="text-xs font-weight-bold"> $14,000 </span>
-                        </td>
-                        <td class="align-middle">
-                           <div class="progress-wrapper w-75 mx-auto">
-                              <div class="progress-info">
-                                 <div class="progress-percentage">
-                                    <span class="text-xs font-weight-bold">60%</span>
-                                 </div>
+                           <div class="d-flex px-2 py-1">
+                              <div>
                               </div>
-                              <div class="progress">
-                                 <div class="progress-bar bg-gradient-info w-60" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+                              <div class="d-flex flex-column justify-content-center">
+                                 <h6 class="mb-0 text-sm"> optional($order->orderEmail)->email</h6>
                               </div>
                            </div>
                         </td>
+                        <td>
+                           <div class="d-flex px-2 py-1">
+                              <div>
+                              </div>
+                              <div class="d-flex flex-column justify-content-center">
+                                 <h6 class="mb-0 text-sm">{{ $order->created_at->format('d-m-y') }}</h6>
+                              </div>
+                           </div>
+                        </td>
+                        @endforeach
+
                      </tr>
 
                   </tbody>
