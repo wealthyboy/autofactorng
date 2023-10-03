dd69bbfc913603686439a3c275f515120c38d1d4
Jacob <jacob.atam@gmail.com>
2023-10-03 03:21:06 -0700

    removed dd

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -41,14 +41,16 @@ class Cart extends Model
     {
         //SELECT ALL FROM THE USER ID && FROM THE USER COOKIE
         $cookie = \Cookie::get('cart');
-        $carts = Cart::with(["product"])->where(['carts.remember_token' => $cookie])->get();
-        static::sync($carts);
+        // $carts = Cart::with(["product"])->where(['carts.remember_token' => $cookie])->get();
+        // static::sync($carts);
 
 
         if (optional(auth()->user())->id) {
             $carts = Cart::with(["product"])->where(['user_id' => optional(auth()->user())->id])->get();
+            static::sync($carts);
         } else {
             $carts = Cart::with(["product"])->where(['carts.remember_token' => $cookie])->get();
+            static::sync($carts);
         }
         return $carts;
     }
