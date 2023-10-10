a4ae8711cdae4bbc91bf064f90e695e6b2afe5a8
Jacob <jacob.atam@gmail.com>
2023-10-10 07:15:27 -0700

    fix

--- resources/views/_partials/auto_cover.blade.php
+++ resources/views/_partials/auto_cover.blade.php
@@ -2,14 +2,14 @@
     <div class="col-md-6 mb-sm-1">
         <a href="/plans?type=auto_cover" class="d-block p-0 ">
             <div class="align-self-center text-center">
-                <img class="img-fluid" src="/images/utils/Autocover4.jpg" alt="" srcset="">
+                <img title="Auto cover " class="img-fluid" src="/images/utils/Autocover4.jpg" alt="auto cover" srcset="">
             </div>
         </a>
     </div>
     <div class="col-md-6 ">
         <a href="/buy-now-pay-later?type=auto_cover" class="d-block p-0 ">
             <div class="align-self-center text-center">
-                <img class="img-fluid" src="/images/utils/Buypaylater.jpg" alt="" srcset="">
+                <img title="buy now pay later" class="img-fluid" src="/images/utils/Buypaylater.jpg" alt="Buy pay later" srcset="">
             </div>
         </a>
     </div>
