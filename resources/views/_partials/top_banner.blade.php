e0e1a34c76f4bf9d4e1ea49d161a20e302f879cf
Jacob <jacob.atam@gmail.com>
2023-10-11 06:23:18 -0700

    Fix

--- resources/views/_partials/top_banner.blade.php
+++ resources/views/_partials/top_banner.blade.php
@@ -1,15 +1,15 @@
 @if (null !== $global_promo)
 <div class="container-fluid text-center mt-3">
     <div class="row">
-        @foreach( $top_banners as  $top_banner) 
-            <div class="col-12">
+        @foreach( $top_banners as $top_banner)
+        <div class="col-12">
             <div class="{{ $top_banner->device }}">
-            <img src="{{ $top_banner->image }}" class=" img-fluid" alt="" srcset="">
+                <img src="{{ $top_banner->image }}" class=" img-fluid" alt="5% percent off banner " title="{{  $top_banner->title  }}" srcset="">
 
             </div>
-        </div>
+        </div>5% percent off banner
         @endforeach
-        
+
     </div><!-- End .container -->
 </div>
 @endif
