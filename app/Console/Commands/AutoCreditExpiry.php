68ce48a01eafcc71133f05e024d197bd04b37b01
Jacob <jacob.atam@gmail.com>
2023-10-16 06:57:13 -0700

    fix

--- app/Console/Commands/AutoCreditExpiry.php
+++ app/Console/Commands/AutoCreditExpiry.php
@@ -43,8 +43,7 @@ class AutoCreditExpiry extends Command
     {
 
         $weeks = Carbon::now()->addDays(14);
-        $month = Carbon::now()->addMonth();
-        $subscribers = Subscribe::has('user')->get();
+        $subscribers = Subscribe::has('user')->where("ends_at", "<", $weeks)->get();
 
         if (null !== $subscribers) {
 
