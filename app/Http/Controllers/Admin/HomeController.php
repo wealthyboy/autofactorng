f4552b43cd2c52b27a043976fa3cbb739a2a54ea
Jacob <jacob.atam@gmail.com>
2023-10-21 06:36:25 -0700

    fix

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -53,10 +53,11 @@ class HomeController extends Controller
 
         foreach ($images as $key => $image) {
             $url = explode('//', $image->image);
-            dd($url);
-            $url = explode('/', $url[1]);
-            $image->image = $url[0] === 'autofactor.ng' ? 'https://autofactorng.com/' . $url[1] . '/' . $url[2] . '/' . $url[3] :  $image->image;
-            $image->save();
+            if (isset($url[1])) {
+                $url = explode('/', $url[1]);
+                $image->image = $url[0] === 'autofactor.ng' ? 'https://autofactorng.com/' . $url[1] . '/' . $url[2] . '/' . $url[3] :  $image->image;
+                $image->save();
+            }
         }
 
 
