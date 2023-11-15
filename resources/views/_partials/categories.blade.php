b2687bb65e4d3e395beb77dd4796b1fd3376db43
Jacob <jacob.atam@gmail.com>
2023-11-14 18:01:20 -0800

    fix

--- resources/views/_partials/categories.blade.php
+++ resources/views/_partials/categories.blade.php
@@ -3,7 +3,7 @@
     <h2 class=" mb-0 mb-3 display-4">FEATURED <strong>CATEGORIES</strong> </h2>
     <div class="underline mb-5 ms-3"></div>
 </div>
-<div class="row g-0">
+<div title="shop all categories " class="row g-0">
     @foreach($featured_categories as $key => $category)
     <div data-animation-name="fadeInUpShorter" itemscope itemtype="https://schema.org/category" class="col-6 col-lg-3 col-md-6 appear-animate">
         <a href="{{ $category->link() }}" class="d-block p-0 border category-content  py-5 no-hover" itemscope itemtype="https://schema.org/Text">
