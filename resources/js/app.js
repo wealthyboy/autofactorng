ef19f111a7da0ba316dedc5622c56bdce6d29445
Jacob <jacob.atam@gmail.com>
2024-06-20 09:30:44 -0700

    fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -115,3 +115,29 @@ $(function () {
 
 
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    let lazyImages = [].slice.call(document.querySelectorAll('img.owl-lazy'));
+    if ('IntersectionObserver' in window) {
+        let lazyImageObserver = new IntersectionObserver(function (entries, observer) {
+            entries.forEach(function (entry) {
+                if (entry.isIntersecting) {
+                    let lazyImage = entry.target;
+                    lazyImage.src = lazyImage.dataset.src;
+                    lazyImage.classList.remove('owl-lazy');
+                    lazyImageObserver.unobserve(lazyImage);
+                }
+            });
+        });
+
+        lazyImages.forEach(function (lazyImage) {
+            lazyImageObserver.observe(lazyImage);
+        });
+    } else {
+        // Fallback for browsers that don't support IntersectionObserver
+        lazyImages.forEach(function (lazyImage) {
+            lazyImage.src = lazyImage.dataset.src;
+        });
+    }
+});
