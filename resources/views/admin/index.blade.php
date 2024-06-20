982ad7dfe767385c892c6ec82308cb891e584ef2
Jacob <jacob.atam@gmail.com>
2024-06-20 10:06:53 -0700

    fix

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -59,7 +59,7 @@
                      <a class="cursor-pointer" id="dropdownTable" data-bs-toggle="dropdown" aria-expanded="false">
                         <i class="fa fa-ellipsis-v text-secondary" aria-hidden="true"></i>
                      </a>
-                     <ul class="dropdown-menu px-2 py-3 ms-sm-n4 ms-n5" aria-labelledby="dropdownTable" style="">
+                     <ul class="dropdown-menu px-2 py-3 ms-sm-n4 ms-n5" aria-labelledby="dropdownTable">
                         <li><a class="dropdown-item border-radius-md" href="javascript:;">Action</a></li>
                         <li><a class="dropdown-item border-radius-md" href="javascript:;">Another action</a></li>
                         <li><a class="dropdown-item border-radius-md" href="javascript:;">Something else here</a></li>
