8a395c7528bae41863689a90c92c22047295e046
Jacob <jacob.atam@gmail.com>
2024-06-20 04:03:34 -0700

    fix

--- resources/views/_partials/brands.blade.php
+++ resources/views/_partials/brands.blade.php
@@ -10,7 +10,7 @@
         <a class="d-block p-0 border">
             <div class="d-flex justify-content-center align-items-center">
                 <div class="d-flex justify-content-center align-items-center text-center image-category">
-                    <img itemprop="image" class="image-class" title="We have genuine {{ $brand->name }} for you" data-src="{{ $brand->image }}" alt="{{ $brand->name }} ">
+                    <img itemprop="image" class="image-class img-fluid" title="We have genuine {{ $brand->name }} for you" data-src="{{ $brand->image }}" alt="{{ $brand->name }} ">
                 </div>
             </div>
         </a>
