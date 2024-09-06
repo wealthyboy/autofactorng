449e90bfe00ec3d164b80367da18c7fd842080ed
Jacob <jacob.atam@gmail.com>
2024-09-06 10:00:40 +0100

    fix

--- app/Models/MakeModelYearEngine.php
+++ app/Models/MakeModelYearEngine.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 
-class MakeModelYear extends Model
+class MakeModelYearEngine extends Model
 {
     use HasFactory;
 
