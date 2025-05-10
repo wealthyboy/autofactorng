69813b656d015d05d4c92b6ef3f1e3abb6a57d0a
Jacob <jacob.atam@gmail.com>
2025-05-10 20:30:25 +0100

    fix

--- resources/views/admin/tracking/show.blade.php
+++ resources/views/admin/tracking/show.blade.php
@@ -8,7 +8,7 @@
 
         <div class="card-header mb-0 d-flex  border-botoom">
             <h5 class="me-2 mb-0">Page URL:</h5>
-            <span class="" style="max-width: 300px;" title="{{ $data->page_url }}">
+            <span class="" title="{{ $data->page_url }}">
                 {{ $data->page_url }}
             </span>
         </div>
