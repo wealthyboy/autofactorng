9c6d50f7a7ec67474a48ab949cf2356dd65182a8
Jacob <jacob.atam@gmail.com>
2025-05-18 20:36:15 +0100

    fix

--- app/Http/Controllers/TrackOrder/TrackOrdersController.php
+++ app/Http/Controllers/TrackOrder/TrackOrdersController.php
@@ -38,7 +38,7 @@ class TrackOrdersController extends Controller
 
         $order_statuses  = Order::$statuses;
 
-        $order = Order::with('order_statuses')->where(
+        $order = Order::where(
             [
                 'invoice' => $request->invoice,
                 'user_id' => auth()->user()->id
