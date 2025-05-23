503da3b065e3f7572fee1ec2a22c4fc481f3f5c1
Jacob <jacob.atam@gmail.com>
2025-05-23 05:22:37 +0100

    fix

--- resources/views/howto/index.blade.php
+++ resources/views/howto/index.blade.php
@@ -28,10 +28,10 @@
         <div class="row how-to justifiy-content-center">
             @foreach($videos as $video)
             <div id="content" class="col-md-6  mb-9 p-">
-                <h2 class="mb-1">{{ $video->title }}</h2>
+                <h2 class="mb-1 fw-bold">{{ $video->title }}</h2>
                 <div style="height: 88px; " class="mt-3  d-none d-lg-block  d-xl-block mb-5"> <?php echo  html_entity_decode($video->description)  ?> </div>
                 <div style="height:" class="mt-3  d-md-none d-lg-none d-sm-block mb-5"> <?php echo  html_entity_decode($video->description)  ?> </div>
-                <div style="height: 69px;  " class="mt-3 d-md-block d-lg-none d-sm-none d-xl-none d-xs-none mb-5"> <?php echo  html_entity_decode($video->description)  ?> </div>
+                <div style="height: 69px;" class="mt-3 d-md-block d-lg-none d-sm-none d-xl-none d-xs-none mb-5"> <?php echo  html_entity_decode($video->description)  ?> </div>
 
                 <?php echo  html_entity_decode($video->link)  ?>
             </div>
