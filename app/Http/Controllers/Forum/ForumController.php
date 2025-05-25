ebb5bb3412680598a73c014b6cbd4e306628622a
Jacob <jacob.atam@gmail.com>
2025-05-25 16:08:20 +0100

    fix

--- app/Http/Controllers/Forum/ForumController.php
+++ app/Http/Controllers/Forum/ForumController.php
@@ -39,7 +39,7 @@ class ForumController extends Controller
             $query->orderBy('id', $request->sort); // Example: top = most viewed
         }
         // Pagination
-        $topics = $query->where('oinned', false)->paginate(10)->withQueryString();
+        $topics = $query->where('pinned', false)->paginate(10)->withQueryString();
 
         $data = [
             'topics' => $topics,
