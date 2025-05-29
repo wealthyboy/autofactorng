35d668ac1a56cf58eeeb0e71cd5866868faa6074
Jacob <jacob.atam@gmail.com>
2025-05-29 11:14:56 +0100

    fix

--- app/Observers/ProductObserver.php
+++ app/Observers/ProductObserver.php
@@ -32,7 +32,7 @@ class ProductObserver
         }
 
         if (!empty($changes)) {
-            Notification::route('mail', 'jacob.atam@gmail.com')
+            Notification::route('mail', 'info@autofactorng.com')
                 ->notify(new ProductUpdated($product, $changes));
         }
     }
