96525313e31d792d5ea28c87d0120236c74d659f
Jacob <jacob.atam@gmail.com>
2025-06-17 13:22:40 +0100

    fix

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -223,7 +223,7 @@ class Order extends Model
 
 	static function appendOrderRow(
 		array  $data,
-		string $sheetName        = 'Order Processing Tracker',
+		string $sheetName        = 'SHEET1',
 		string $valueInputOption = 'RAW',        // or USER_ENTERED
 		string $insertDataOption = 'OVERWRITE'   // or INSERT_ROWS
 	): void {
