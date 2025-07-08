c4341c13a35411126e9dae209072c2e4134ee9d2
Jacob <jacob.atam@gmail.com>
2025-07-09 00:10:07 +0100

    fix

--- resources/js/components/checkout/CheckoutIndex.vue
+++ resources/js/components/checkout/CheckoutIndex.vue
@@ -308,7 +308,7 @@ export default {
         checkoutWithLagos: function (e) {
 
             if (this.total >= 300000) {
-                alert("No Payment On Delivery On Orders Above ₦300,000.\nKindly use the pay now option");
+                alert("No Payment On Delivery On Orders Above ₦300,000.\nKindly Use The Pay Now Option.");
                 return;
             }
 
