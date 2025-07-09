d3ac590a2a7e4d8ffb4b42a2180c975c17e3ea14
Jacob <jacob.atam@gmail.com>
2025-07-09 14:12:17 +0100

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -277,12 +277,12 @@
                         <a data-bs-toggle="offcanvas" data-bs-target="#offcanvas" class="nav-btn menu-nav-btn mb-0 pb-0 border-0" role="button">
                            <span class="menu-open d-flex flex-column align-items-center">
                               <img data-bs-toggle="offcanvas" data-bs-target="#offcanvas" src="/images/utils/hamburger.svg" alt="hamburger" title="Click here to start shopping auto parts in nigeria" class="ms-1" srcset="">
-                              <div class="">Menu</div>
+                              <div class="bold">Menu</div>
                            </span>
 
                            <span class="menu-close d-none d-flex flex-column align-items-center">
                               <img data-bs-toggle="offcanvas" alt="Close" data-bs-target="#offcanvas" src="/images/utils/close-dark.svg" class="ms-2" alt="close button" srcset="">
-                              <div class="text-xs">Close</div>
+                              <div class="text-xs bold">Close</div>
                            </span>
                         </a>
                      </div>
