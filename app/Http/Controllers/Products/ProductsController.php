2731db12b813cb5bb726066420843d1ee02c9c4a
Jacob <jacob.atam@gmail.com>
2025-07-10 21:29:33 +0100

    fix

--- app/Http/Controllers/Products/ProductsController.php
+++ app/Http/Controllers/Products/ProductsController.php
@@ -113,17 +113,10 @@ class ProductsController extends Controller
 
         $product = Product::where(function ($query) use ($request) {
             $keywords = preg_split('/\s+/', $request->q); // Split into words by space
-            dd($keywords);
             foreach ($keywords as $word) {
                 $query->whereRaw("REPLACE(LOWER(name), '-', '') LIKE ?", ['%' . strtolower(str_replace('-', '', $word)) . '%']);
             }
-        })
-            ->whereHas('categories', function (Builder $builder) {
-                $builder->where(function ($q) {
-                    $q->where('categories.slug', 'spare-parts')
-                        ->orWhere('categories.slug', 'servicing-parts');
-                });
-            });
+        });
 
 
 
