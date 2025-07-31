d23717cc5d006cdb78cc07016337d333b92fde99
Jacob <jacob.atam@gmail.com>
2025-07-31 07:55:56 +0100

    fix

--- app/Models/AbandonedCart.php
+++ app/Models/AbandonedCart.php
@@ -20,4 +20,9 @@ class AbandonedCart extends Model
         'checkout_started_at',
         'recovered',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
