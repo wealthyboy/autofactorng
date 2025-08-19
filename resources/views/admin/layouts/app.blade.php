c4e4bd63fa94439c063eea0f107cb664acc59011
Jacob <jacob.atam@gmail.com>
2025-08-19 19:15:30 +0100

    fix

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -8,6 +8,8 @@
    <meta charset="utf-8" />
 
    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+   <meta name="robots" content="noindex, nofollow">
+
    <meta name="csrf-token" content="{{ csrf_token() }}">
    <title>{{ Config('app.name')}} | Admin</title>
    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
