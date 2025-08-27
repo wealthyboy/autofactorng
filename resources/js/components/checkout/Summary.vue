27f75ccbb164d663c86d81a19663558c980c91e9
Jacob <jacob.atam@gmail.com>
2025-08-27 16:00:07 +0100

    fix

--- resources/js/components/checkout/Summary.vue
+++ resources/js/components/checkout/Summary.vue
@@ -99,10 +99,12 @@
             <h5 class="mb-3 font-weight-bold">Shipping Options</h5>
 
             <div class="list-group">
-            <label  :id="index" v-for="(method, index) in prices.zones"
+            <label    :for="`shipping-${index}`"
+ v-for="(method, index) in prices.zones"
                     :key="index" class="list-group-item d-flex justify-content-between align-items-center">
                 <div>
-                <input  :id="index" @change="emitPrice(method)"
+                <input        :id="`shipping-${index}`"
+ @change="emitPrice(method)"
                     v-model="selectedMethod"
                     :value="method.price" type="radio" name="shipping" class="custom-control-input">
                 <span class="font-weight-bold">{{ method.zone }}</span>
