3eb4066ac813caecfe707d4fbe7e1db961d68efe
Jacob <jacob.atam@gmail.com>
2025-09-01 05:51:45 +0100

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,44 +1,16 @@
-# Change to the project directory
+cd /home/forge/autofactorng.com
 
-cd /var/www/autofactorng.com
+git pull origin $FORGE_SITE_BRANCH
+$FORGE_COMPOSER install --no-dev --no-interaction --prefer-dist --optimize-autoloader
 
-php artisan down || true
+# Prevent concurrent php-fpm reloads...
+touch /tmp/fpmlock 2>/dev/null || true
+( flock -w 10 9 || exit 1
+    echo 'Reloading PHP FPM...'; sudo -S service $FORGE_PHP_FPM reload ) 9</tmp/fpmlock
 
-git pull origin master --no-edit
+npm ci && npm run build
 
-# Turn on maintenance mode
-
-# Pull the latest changes from the git repository
-# git reset --hard
-# git clean -df
-#git pull origin master
-
-# Install/update composer dependecies
-composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
-
-# Run database migrations
-php artisan migrate --force
-
-# Clear caches
-php artisan cache:clear
-
-# Clear expired password reset tokens
-php artisan auth:clear-resets
-
-# Clear and cache routes
-php artisan route:cache
-
-# Clear and cache config
-php artisan config:cache
-
-# Clear and cache views
-php artisan view:cache
-
-# Install node modules
-#npm install
-
-# Build assets using Laravel Mix
-#:wqnpm run prod
-
-# Turn off maintenance mode
-php artisan up
+if [ -f artisan ]; then
+    $FORGE_PHP artisan optimize
+    $FORGE_PHP artisan migrate --force
+fi
