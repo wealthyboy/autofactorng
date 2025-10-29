0ac9cec097fb2a9ca8d517619ea85496cd228864
Jacob <jacob.atam@gmail.com>
2025-10-29 11:36:50 +0100

    fix

--- app/Http/Controllers/Admin/Orders/OrdersController.php
+++ app/Http/Controllers/Admin/Orders/OrdersController.php
@@ -51,17 +51,19 @@ class OrdersController extends Table
 
 		$orders = Order::has('ordered_products')->orderBy('created_at', 'desc')->paginate(150);
 		$orders = $this->getColumnListings(request(), $orders);
+		if (request()->debug) {
+			dd(
+				$orders = Order::query()
+					->has('user')
+					->where('allow_review', 1)
+					->where('email', 'damilola@autofactorng.com')
+					->whereDate('created_at', \Carbon::today())
+					->get()
+			);
+		}
 
-		dd(
-			$orders = Order::query()
-            ->has('user')
-            ->where('allow_review', 1)
-            ->where('email', 'damilola@autofactorng.com')
-            ->whereDate('created_at', \Carbon::today())
-            ->get()
-		);
 
-		
+
 		return view('admin.orders.index', compact('orders'));
 	}
 
