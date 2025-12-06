b508a809b684ea99e22b6f0b502858dc25d35379
Jacob <jacob.atam@gmail.com>
2025-12-06 10:36:40 +0100

    fix

--- app/Notifications/ProductReviewNotification.php
+++ app/Notifications/ProductReviewNotification.php
@@ -52,11 +52,7 @@ class ProductReviewNotification extends Notification
                 'emails.products_review.index',
                 ['u' => $this->user, 'order' => $this->order],
             )
-            ->bcc("jacob.atam@gmail.com")
             ->bcc("info@autofactorng.com")
-
-
-
             ->subject('Review our product');
     }
 
