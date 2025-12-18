d5adcbc4a457cbb9650d1c24886ddd7030a7ec72
Jacob <jacob.atam@gmail.com>
2025-12-18 11:25:53 +0100

    fix

---
+++ resources/views/emails/test.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Mail Test</title>
+</head>
+
+<body>
+    <h2>Mail Test Successful 🎉</h2>
+    <p>If you are seeing this email, your server mail configuration is working.</p>
+    <p><strong>Time:</strong> {{ now() }}</p>
+</body>
+
+</html>
