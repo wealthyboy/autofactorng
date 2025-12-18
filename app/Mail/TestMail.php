58cf36c79c9331188e0eeaee9b51923de76d1e7c
Jacob <jacob.atam@gmail.com>
2025-12-18 11:30:56 +0100

    fix

--- app/Mail/TestMail.php
+++ app/Mail/TestMail.php
@@ -3,7 +3,6 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
@@ -11,23 +10,11 @@ class TestMail extends Mailable
 {
     use Queueable, SerializesModels;
 
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
+    public function __construct() {}
 
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->subject('✅ Server Mail Test')
+            ->view('emails.test');
     }
 }
