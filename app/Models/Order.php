13e30c5b8ecc82a6da5f1c9eaf22c9965ba5161d
Jacob <jacob.atam@gmail.com>
2025-12-18 15:19:59 +0100

    fix

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -411,9 +411,9 @@ class Order extends Model
 
 		try {
 			$when = now()->addMinutes(5);
-			// Mail::to($user->email)
-			// 	->bcc('order@autofactorng.com')
-			// 	->send(new OrderReceipt($order, null, null, $sub_total));
+			Mail::to($user->email)
+				->bcc('order@autofactorng.com')
+				->queue(new OrderReceipt($order, null, null, $sub_total));
 		} catch (\Throwable $th) {
 			Log::info("Mail error :" . $th);
 			$err = new Error();
