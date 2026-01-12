ceb5883782855557d344a2b2b3384374d322d903
Jacob <jacob.atam@gmail.com>
2026-01-12 12:53:55 +0100

    fix

--- resources/views/pages/returns.blade.php
+++ resources/views/pages/returns.blade.php
@@ -30,14 +30,16 @@
                 We’re here to help make your return process quick and easy.
             </p>
 
-            <p>Please click the link below to learn more about our Return, Refund, and Wallet Policy.
-            <h2 style="font-style: italic;"><span style="font-family: Tahoma, Geneva, sans-serif; font-size: medium;"><ins><strong><a href="https://autofactorng.com/pages/return-policy"><span style="color: rgb(52, 152, 219);">Learn More</span></a></strong></ins></span></h2>
-
-            </p>
             <p>
                 Do you have a Return? Kindly click on the WhatsApp link to send us to message and a customer care representative would respond to you as soon as possible.
             <h2 style="font-style: italic;"><span style="font-family: Tahoma, Geneva, sans-serif; font-size: medium;">Whatsapp- <ins><strong><a href="https://cbl.link/ZrFXBpf"><span style="color: rgb(52, 152, 219);">Click to send us a Message</span></a></strong></ins></span></h2>
             </p>
+
+            <p>Please click the link below to learn more about our Return, Refund, and Wallet Policy.
+            <h2 style="font-style: italic;"><span style="font-family: Tahoma, Geneva, sans-serif; font-size: medium;"><ins><strong><a href="https://autofactorng.com/pages/return-policy"><span style="color: rgb(52, 152, 219);">Learn More</span></a></strong></ins></span></h2>
+            =
+            </p>
+
             <div class="margin-top-35"></div>
         </div> <!-- /row -->
     </div> <!-- /container -->
