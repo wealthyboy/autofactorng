cb85edc98b0646f4486f68f120745a1b80f7a5fa
Jacob <jacob.atam@gmail.com>
2026-06-18 17:50:52 +0100

    fix

---
+++ database/migrations/2026_06_18_164757_alter_stock_snapshots_change_batch_id.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterStockSnapshotsChangeBatchId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('stock_snapshots', function (Blueprint $table) {
+            $table->string('batch_id')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('stock_snapshots', function (Blueprint $table) {
+            //
+        });
+    }
+}
